c55d84c12ee83e3f78593607b3e615b9ab1e6c98
Dmitriy Voronin <dvoronin@avito.ru>
2020-01-06 15:10:51 +0300

    убрал из зависимостей agp, иначе проникает в avito android test config

--- utils/build.gradle.kts
+++ utils/build.gradle.kts
@@ -16,8 +16,9 @@ dependencies {
     implementation(gradleApi())
     implementation(project(":kotlin-dsl-support"))
     implementation("org.jetbrains.kotlin:kotlin-stdlib:${kotlinVersion}")
-    implementation("com.android.tools.build:gradle:$androidGradlePluginVersion")
     implementation("io.github.azagniotov:ant-style-path-matcher:$antPatternMatcherVersion")
+
+    compileOnly("com.android.tools.build:gradle:$androidGradlePluginVersion")
 
     testImplementation("com.google.truth:truth:$truthVersion")
     testImplementation("com.nhaarman.mockitokotlin2:mockito-kotlin:$mockitoKotlin2Version")
