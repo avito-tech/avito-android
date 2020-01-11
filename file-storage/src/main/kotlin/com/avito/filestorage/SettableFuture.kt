172a388d6adfbb9be1dfc9274c0c81a4f65b4d4a
Eugene Krivobokov <eugene.krivobokov@gmail.com>
2020-01-11 23:02:51 +0300

    Add file-storage module (#27)

---
+++ file-storage/src/main/kotlin/com/avito/filestorage/SettableFuture.kt
@@ -0,0 +1,34 @@
+package com.avito.filestorage
+
+import java.util.concurrent.CountDownLatch
+import java.util.concurrent.atomic.AtomicBoolean
+
+interface FutureValue<T> {
+    fun get(): T
+}
+
+internal class SettableFutureValue<T> : FutureValue<T> {
+    private val latch = CountDownLatch(1)
+    private val isSet = AtomicBoolean()
+    private var slot: T? = null
+
+    override fun get(): T {
+        latch.await()
+        return slot!!
+    }
+
+    fun set(value: T): Boolean {
+        if (isSet.compareAndSet(false, true)) {
+            slot = value
+            latch.countDown()
+
+            return true
+        }
+
+        return false
+    }
+}
+
+fun <T, R> FutureValue<T>.map(transformer: (T) -> R): FutureValue<R> = object : FutureValue<R> {
+    override fun get(): R = transformer(this@map.get())
+}
---
+++ file-storage/src/main/kotlin/com/avito/filestorage/ToStringConverterFactory.kt
@@ -0,0 +1,35 @@
+package com.avito.filestorage
+
+import okhttp3.MediaType
+import okhttp3.RequestBody
+import okhttp3.ResponseBody
+import retrofit2.Converter
+import retrofit2.Retrofit
+import java.lang.reflect.Type
+
+internal class ToStringConverterFactory : Converter.Factory() {
+
+    private val uriRegex = Regex("uri\":.*\"(.+)\"")
+    private val mediaType = MediaType.parse("text/html")
+
+    override fun responseBodyConverter(
+        type: Type?,
+        annotations: Array<Annotation>?,
+        retrofit: Retrofit?
+    ): Converter<ResponseBody, *>? {
+        return if (String::class.java == type) {
+            Converter<ResponseBody, String> { value -> uriRegex.find(value.string())?.groupValues?.getOrNull(1) }
+        } else null
+    }
+
+    override fun requestBodyConverter(
+        type: Type?,
+        parameterAnnotations: Array<out Annotation>?,
+        methodAnnotations: Array<out Annotation>?,
+        retrofit: Retrofit?
+    ): Converter<*, RequestBody>? {
+        return if (String::class.java == type) {
+            Converter<String, RequestBody> { value -> RequestBody.create(mediaType, value) }
+        } else null
+    }
+}
