106cd3d9ba7ba28cb33aeb871ede07f4371d0718
Dmitriy Voronin <dvoronin@avito.ru>
2020-01-17 14:13:20 +0300

    add :qapps module

---
+++ qapps/src/main/kotlin/com/avito/plugin/QAppsUploadTask.kt
@@ -0,0 +1,68 @@
+package com.avito.plugin
+
+import com.avito.utils.BuildFailer
+import com.avito.utils.logging.ciLogger
+import org.gradle.api.DefaultTask
+import org.gradle.api.file.RegularFileProperty
+import org.gradle.api.model.ObjectFactory
+import org.gradle.api.tasks.Input
+import org.gradle.api.tasks.InputFile
+import org.gradle.api.tasks.TaskAction
+import org.gradle.kotlin.dsl.property
+import javax.inject.Inject
+
+/**
+ * TODO specify outputs: write a file after success upload (with link for example)
+ * TODO check caching
+ */
+@Suppress("UnstableApiUsage")
+abstract class QAppsUploadTask @Inject constructor(objects: ObjectFactory) : DefaultTask() {
+
+    @InputFile
+    abstract fun getApk(): RegularFileProperty // setup in ci build step
+
+    @Input
+    val comment = objects.property<String>()
+
+    @Input
+    val branch = objects.property<String>()
+
+    @Input
+    val host = objects.property<String>()
+
+    @Input
+    val versionName = objects.property<String>()
+
+    @Input
+    val versionCode = objects.property<String>()
+
+    @Input
+    val packageName = objects.property<String>()
+
+    @Input
+    val releaseChain = objects.property<Boolean>()
+
+    @TaskAction
+    fun upload() {
+        val apk = getApk().asFile.get()
+
+        val uploadResult = QAppsUploadAction(
+            apk = apk,
+            comment = comment.get(),
+            host = host.get(),
+            branch = branch.get(),
+            versionName = versionName.get(),
+            versionCode = versionCode.get(),
+            packageName = packageName.get(),
+            releaseChain = releaseChain.get(),
+            logger = project.ciLogger
+        ).upload()
+
+        val buildFailer = BuildFailer.RealFailer()
+
+        uploadResult.fold(
+            { logger.info("upload to qapps successfully: ${apk.path}") },
+            { buildFailer.failBuild("Can't upload to qapps: ${apk.path}; ${it.message}") }
+        )
+    }
+}
---
+++ qapps/src/test/kotlin/com/avito/plugin/QAppsUploadActionTest.kt
@@ -0,0 +1,106 @@
+package com.avito.plugin
+
+import com.avito.utils.logging.CILogger
+import com.google.common.truth.Truth.assertThat
+import com.google.common.truth.Truth.assertWithMessage
+import okhttp3.mockwebserver.Dispatcher
+import okhttp3.mockwebserver.MockResponse
+import okhttp3.mockwebserver.MockWebServer
+import okhttp3.mockwebserver.RecordedRequest
+import org.funktionale.tries.Try
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.io.TempDir
+import java.io.File
+import java.nio.file.Path
+
+class QAppsUploadActionTest {
+
+    private lateinit var testProjectDir: File
+
+    private val server = MockWebServer()
+
+    private val apk: File
+        get() {
+            val apk = File(testProjectDir, "test.apk")
+            apk.createNewFile()
+            apk.writeText("content")
+            return apk
+        }
+
+    private val action: QAppsUploadAction
+        get() = QAppsUploadAction(
+            apk = apk,
+            comment = "comment",
+            host = server.url("/").toString(),
+            branch = "develop",
+            versionName = "version_name",
+            versionCode = "0",
+            packageName = "com.avito.android",
+            releaseChain = false,
+            logger = CILogger.allToStdout
+        )
+
+    @BeforeEach
+    fun setup(@TempDir tempPath: Path) {
+        testProjectDir = tempPath.toFile()
+        server.start()
+    }
+
+    @AfterEach
+    fun tearDown() {
+        server.shutdown()
+    }
+
+    private val failedResponse = MockResponse()
+        .setResponseCode(500)
+        .setBody("[error reason]")
+    private val successResponse = MockResponse().setResponseCode(200)
+
+    @Test
+    fun `action failed - when http request failed all attempts`() {
+        server.setDispatcher(object : Dispatcher() {
+            override fun dispatch(request: RecordedRequest): MockResponse {
+                return failedResponse
+            }
+        })
+
+        val result = action.upload()
+
+        assertThat(result).isInstanceOf(Try.Failure::class.java)
+        val failure = (result as Try.Failure).throwable
+        assertThat(failure.message).contains("Can't upload apk to qapps")
+        assertThat(failure.message).contains("[error reason]")
+
+        assertWithMessage("retry to send")
+            .that(server.requestCount).isAtLeast(2)
+    }
+
+    @Test
+    fun `action success - successful request after failed`() {
+        server.enqueue(failedResponse)
+        server.enqueue(successResponse)
+
+        val result = action.upload()
+
+        assertThat(result).isInstanceOf(Try.Success::class.java)
+        assertThat(server.requestCount).isEqualTo(2)
+    }
+
+    @Test
+    fun `action success - request contains passed params`() {
+        server.enqueue(successResponse)
+
+        val result = action.upload()
+
+        assertThat(server.requestCount).isEqualTo(1)
+        val recordedRequest = server.takeRequest()
+
+        assertThat(recordedRequest.path).isEqualTo("/qapps/api/os/android/upload")
+        assertThat(recordedRequest.body.readUtf8()).contains("content")
+
+        assertThat(result).isInstanceOf(Try.Success::class.java)
+    }
+
+}
