e9a57cae8e8825d858087ff15284093cf886c24d
Dmitriy Voronin <dsvoronin@gmail.com>
2020-01-17 16:08:35 +0300

    add :docs module (#53)

---
+++ docs/build.gradle.kts
@@ -0,0 +1,27 @@
+plugins {
+    id("kotlin")
+    id("java-gradle-plugin")
+    `maven-publish`
+}
+
+val kotlinVersion: String by project
+
+dependencies {
+    implementation(project(":kotlin-dsl-support"))
+    implementation(project(":utils"))
+    implementation(project(":docker"))
+    implementation(project(":sentry"))
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:$kotlinVersion")
+    implementation("org.jetbrains.kotlin:kotlin-reflect:$kotlinVersion")
+    implementation("com.fkorotkov:kubernetes-dsl:1.2.1")
+    implementation("io.fabric8:kubernetes-client:4.1.3")
+}
+
+gradlePlugin {
+    plugins {
+        create("docsDeployPlugin") {
+            id = "com.avito.android.docs"
+            implementationClass = "com.avito.android.plugin.DocsPlugin"
+        }
+    }
+}
