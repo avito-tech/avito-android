00ed8c95768a8c0e2c0915fb1709497604689788
Eugene Krivobokov <eugene.krivobokov@gmail.com>
2020-01-22 21:20:05 +0300

    MBS-7405 Fix local docs publishing (#70)

--- docs/publish.sh
+++ docs/publish.sh
@@ -1,14 +1,17 @@
 #!/usr/bin/env bash
 
 # Build and publish documentation to github pages
+# Prerequisites:
+# - valid ssh key to access: https://help.github.com/en/github/authenticating-to-github/connecting-to-github-with-ssh
+# - export GITHUB_GIT_USER_NAME
+# - export GITHUB_GIT_USER_EMAIL
 
 set -euf -o pipefail
 
 if [ "$(git status -s)" ]
 then
     echo "The working directory is dirty. Please commit any pending changes. TODO: fail"
-    # TODO: fail
-    # exit 1;
+    exit 1;
 fi
 
 cd docs/
@@ -24,3 +27,7 @@ docker run --rm \
         -w="/app" \
         --entrypoint docs/publish_entrypoint.sh \
         android/docs/local:latest
+
+# TODO: simulate the same user in image
+echo "Fix broken from docker permissions in .git"
+sudo chown -R "$(whoami)" .git/
