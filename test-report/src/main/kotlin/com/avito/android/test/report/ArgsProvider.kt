20855af5a0a4d43dd850b115623b6c4966b7da7a
Dmitriy Voronin <dvoronin@avito.ru>
2020-01-28 23:46:23 +0300

    fix empty instrumentation arg "inHouse"
    revert orchestrator detection logic and improved it's error output

--- test-report/src/main/kotlin/com/avito/android/test/report/ArgsProvider.kt
+++ test-report/src/main/kotlin/com/avito/android/test/report/ArgsProvider.kt
@@ -35,7 +35,9 @@ class BundleArgsProvider(
     override fun getMandatoryArgument(name: String): String {
         val result: String? = bundle.getString(name)
         if (result == null || result.isBlank()) {
-            throw ReporterException("$name is a mandatory argument; available keys=${bundle.keySet()}")
+            throw ReporterException(
+                "$name is a mandatory argument; available keys=${bundle.keySet().joinToString(separator = ", ")}"
+            )
         } else {
             return result
         }
