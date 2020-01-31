d833ff6477f44605596dbb5a547013e0ee2fd2e5
Dmitriy Voronin <dsvoronin@gmail.com>
2020-01-31 15:50:38 +0300

    MBS-7578: remove unused dependencies of okhttp module (#129)

--- okhttp/build.gradle.kts
+++ okhttp/build.gradle.kts
@@ -9,14 +9,11 @@ val okhttpVersion: String by project
 val retrofitVersion: String by project
 
 dependencies {
-    implementation(project(":utils"))
-    implementation(project(":kotlin-dsl-support"))
     implementation("org.jetbrains.kotlin:kotlin-stdlib:$kotlinVersion")
-    implementation("org.funktionale:funktionale-try:$funktionaleVersion")
     implementation("com.squareup.okhttp3:okhttp:$okhttpVersion")
-    implementation("com.squareup.okhttp3:logging-interceptor:$okhttpVersion")
 
     testImplementation(project(":test-okhttp"))
+    testImplementation("org.funktionale:funktionale-try:$funktionaleVersion")
     testImplementation("com.squareup.okhttp3:mockwebserver:$okhttpVersion")
     testImplementation("com.squareup.retrofit2:retrofit:$retrofitVersion")
     testImplementation("com.squareup.retrofit2:converter-gson:$retrofitVersion")
