4f5a474033dd1ae5c7374be98f6611c90fe17361
Dmitriy Voronin <dvoronin@avito.ru>
2020-02-10 20:18:46 +0300

    fix https://github.com/gradle/gradle/issues/11308

--- publish_local.sh
+++ publish_local.sh
@@ -2,4 +2,4 @@
 
 source $(dirname $0)/_main.sh
 
-runInBuilder "./gradlew publishToArtifactory ${GRADLE_ARGS}"
+runInBuilder "./gradlew publishToArtifactory ${GRADLE_ARGS} -Dorg.gradle.internal.publish.checksums.insecure=true"
