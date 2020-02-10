f658d10fbdd8ed4c8292edced35fcb907d252ddc
Dmitriy Voronin <dvoronin@avito.ru>
2020-02-10 16:27:03 +0300

    fix command

--- build.sh
+++ build.sh
@@ -6,6 +6,6 @@ TEMP_PROJECT_VERSION="temp-version"
 
 source $(dirname $0)/_main.sh "${GIT_COMMANDS}
     ./gradlew build publishToMavenLocal ${GRADLE_ARGS} -PprojectVersion=${TEMP_PROJECT_VERSION};
-    :subprojects:android-test:instrumentationUi -PinfraVersion=${TEMP_PROJECT_VERSION}"
+    :subprojects:android-test:test-app:instrumentationUi -PinfraVersion=${TEMP_PROJECT_VERSION}"
 
 docs/check.sh
