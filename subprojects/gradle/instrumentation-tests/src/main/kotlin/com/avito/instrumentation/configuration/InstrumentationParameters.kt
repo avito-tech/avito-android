fb76348bf8c7122c2555e804d361aaa10b70c3d8
Sergey Boishtyan <ssboishtyan@avito.ru>
2020-02-19 19:22:06 +0300

    validate instrumentation configuration that can break build (#193)
    * validate instrumentation configuration that can break build
    
    * reuse validate function
    add InstrumentationParameters.kt initialParams validation
    improve AdbDevice.kt comment
    
    Co-authored-by: Dmitriy Voronin <dsvoronin@gmail.com>

--- subprojects/gradle/instrumentation-tests/src/main/kotlin/com/avito/instrumentation/configuration/InstrumentationParameters.kt
+++ subprojects/gradle/instrumentation-tests/src/main/kotlin/com/avito/instrumentation/configuration/InstrumentationParameters.kt
@@ -7,12 +7,26 @@ data class InstrumentationParameters(
     val initialParameters: Map<String, String> = emptyMap()
 ) : Map<String, String> by initialParameters, Serializable {
 
-    fun applyParameters(newParameters: Map<String, String>): InstrumentationParameters =
-        InstrumentationParameters(
+    init {
+        validateParameters(initialParameters)
+    }
+
+    fun applyParameters(newParameters: Map<String, String>): InstrumentationParameters {
+        validateParameters(newParameters)
+        return InstrumentationParameters(
             HashMap(this).apply {
                 putAll(newParameters)
             }
         )
+    }
+
+    private fun validateParameters(parameters: Map<String, String>) {
+        parameters.forEach { (key, value) ->
+            require(key.isNotBlank() && value.isNotBlank()) {
+                "pair key=$key, value=$value has blank string"
+            }
+        }
+    }
 
     fun reportCoordinates(): ReportCoordinates = ReportCoordinates(
         planSlug = getValue("planSlug"),
