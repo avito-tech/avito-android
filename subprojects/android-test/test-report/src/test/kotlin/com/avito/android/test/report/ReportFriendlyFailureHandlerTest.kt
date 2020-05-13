98a8541a3f796c61063c7efe75502e36d716e5c1
Dmitriy Voronin <dsvoronin@gmail.com>
2020-05-13 17:16:41 +0300

    MBS-8303 remove viewHierarchy from NoMatchingViewException verdict (#382)
    * MBS-8303 remove viewHierarchy from NoMatchingViewException verdict

---
+++ subprojects/android-test/test-report/src/test/kotlin/com/avito/android/test/report/ReportFriendlyFailureHandlerTest.kt
@@ -0,0 +1,24 @@
+package com.avito.android.test.report
+
+import androidx.test.espresso.NoMatchingViewException
+import com.avito.android.util.fileFromJarResources
+import com.google.common.truth.Truth.assertThat
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+
+internal class ReportFriendlyFailureHandlerTest {
+
+    @Test
+    fun `failureHandler - removes view hierarchy`() {
+        val exception = assertThrows<NoMatchingViewException> {
+            ReportFriendlyFailureHandler(StubFailureHandler).handle(
+                error = createExceptionWithPrivateStringConstructor<NoMatchingViewException>(
+                    fileFromJarResources<ReportFriendlyFailureHandlerTest>("view-hierarchy.txt").readText()
+                ),
+                viewMatcher = null
+            )
+        }
+
+        assertThat(exception.message).isEqualTo("Не найдена view в иерархии")
+    }
+}
