79fa5aa944cbc1f22b5809c473c506e12e7eac0c
Sergey Boishtyan <ssboishtyan@avito.ru>
2020-05-13 15:22:15 +0300

    MBS-8441 Fix gitState computation caching

--- subprojects/gradle/kotlin-dsl-support/src/main/kotlin/com/avito/kotlin/dsl/ProvidersExtensions.kt
+++ subprojects/gradle/kotlin-dsl-support/src/main/kotlin/com/avito/kotlin/dsl/ProvidersExtensions.kt
@@ -4,8 +4,6 @@ import org.gradle.api.internal.provider.Providers
 import org.gradle.api.provider.Provider
 import org.gradle.api.provider.ProviderFactory
 
-/**
- * В отличие от [ProviderFactory.provider] закеширует первое значение.
- */
 @Suppress("UnstableApiUsage")
+@Deprecated("It behaves like simple provider", replaceWith = ReplaceWith("[ProviderFactory.provider]"))
 fun <T> ProviderFactory.lazy(factory: () -> T): Provider<T> = Providers.of(Unit).map { factory() }
