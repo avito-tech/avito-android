6dbb772b7e34d968936bd6ab130d37dc6d4cc2b4
Dmitriy Voronin <dvoronin@avito.ru>
2020-05-25 17:01:04 +0300

    MBS-8007 remove depreacated ProviderFactory.lazy

--- subprojects/gradle/sentry-config/src/main/kotlin/com/avito/android/sentry/ProjectExtensions.kt
+++ subprojects/gradle/sentry-config/src/main/kotlin/com/avito/android/sentry/ProjectExtensions.kt
@@ -7,28 +7,27 @@ import com.avito.kotlin.dsl.ProjectProperty
 import com.avito.kotlin.dsl.PropertyScope.ROOT_PROJECT
 import com.avito.kotlin.dsl.getBooleanProperty
 import com.avito.kotlin.dsl.getMandatoryStringProperty
-import com.avito.kotlin.dsl.lazy
 import com.avito.kotlin.dsl.lazyProperty
 import io.sentry.SentryClient
 import org.gradle.api.Project
 import org.gradle.api.provider.Provider
 
 fun Project.environmentInfo(): Provider<EnvironmentInfo> = lazyProperty("ENVIRONMENT_INFO_PROVIDER") { project ->
-    project.providers.lazy {
+    project.providers.provider {
         val git = Git.Impl(project.rootDir, project.logger::info)
         EnvironmentInfoImpl(project, git)
     }
 }
 
 val Project.sentry: Provider<SentryClient> by ProjectProperty.lazy(scope = ROOT_PROJECT) { project ->
-    project.providers.lazy {
+    project.providers.provider {
         val config = from(project)
         sentryClient(config)
     }
 }
 
 val Project.sentryConfig: Provider<SentryConfig> by ProjectProperty.lazy(scope = ROOT_PROJECT) { project ->
-    project.providers.lazy {
+    project.providers.provider {
         from(project)
     }
 }
