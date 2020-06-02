573f2265b8bdb95a7901a331957689cb34461b7c
Daniil Popov <int02h@gmail.com>
2020-06-02 16:48:36 +0300

    MBS-6859 exclude emulator28 from fast and full check emulator set

--- subprojects/gradle/instrumentation-tests/src/main/kotlin/com/avito/instrumentation/EmulatorSet.kt
+++ subprojects/gradle/instrumentation-tests/src/main/kotlin/com/avito/instrumentation/EmulatorSet.kt
@@ -5,12 +5,12 @@ import com.avito.instrumentation.reservation.request.Device
 object EmulatorSet {
     val fast = setOf(
         Device.Emulator.Emulator22,
-        Device.Emulator.Emulator28
+        Device.Emulator.Emulator27
     )
     val full = setOf(
         Device.Emulator.Emulator22,
         Device.Emulator.Emulator23,
         Device.Emulator.Emulator24,
-        Device.Emulator.Emulator28
+        Device.Emulator.Emulator27
     )
 }
