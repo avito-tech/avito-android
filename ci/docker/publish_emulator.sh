c54a820e8f4cf148d86286d447631e54fedd87cf
Dmitriy Voronin <dvoronin@avito.ru>
2020-06-05 16:28:15 +0300

    MBS-8552 build emulators one-by-one with API parameter
    something wrong with building in same process (it takes much more time than expected)

--- ci/docker/publish_emulator.sh
+++ ci/docker/publish_emulator.sh
@@ -1,13 +1,21 @@
 #!/usr/bin/env bash
 
-source $(dirname $0)/../_environment.sh
+BUILD_DIRECTORY=$1
+API=$2
 
-BUILD_DIRECTORY=$(pwd)/$1
+if [ -n "$BUILD_DIRECTORY" ] && [ -n "$API" ]; then
+    echo "building image for API=$API"
+else
+    echo "'BUILD_DIRECTORY' and 'API' parameters should be provided. Example ./publish_emulator android_emulator 22. See available API's in ./android_emulator/hardware/"
+    exit 1
+fi
+
+source $(dirname $0)/../_environment.sh
 
 docker run --rm \
     --volume /var/run/docker.sock:/var/run/docker.sock \
-    --volume ${BUILD_DIRECTORY}:/build \
+    --volume "$(pwd)/$BUILD_DIRECTORY":/build \
     --env "DOCKER_REGISTRY=${DOCKER_REGISTRY}" \
     --env DOCKER_LOGIN=${DOCKER_LOGIN} \
     --env DOCKER_PASSWORD=${DOCKER_PASSWORD} \
-    ${IMAGE_DOCKER_IN_DOCKER} publish_docker_image publish_emulator /build "22 23 24 27 28"
+    ${IMAGE_DOCKER_IN_DOCKER} publish_docker_image publish_emulator /build "${API}"
