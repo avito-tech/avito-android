f2e91574de981aeafe9e2f56eec32b8a4d9ff6e1
Dmitriy Voronin <dvoronin@avito.ru>
2020-07-14 17:48:44 +0300

    MBS-8580 remove dependencies lint plugin

--- subprojects/gradle/dependencies-lint/src/main/kotlin/com/avito/android/lint/dependency/UnknownComponent.kt
+++
@@ -1,9 +0,0 @@
-package com.avito.android.lint.dependency
-
-import org.gradle.api.artifacts.component.ComponentIdentifier
-
-internal class UnknownComponent(val description: String): ComponentIdentifier {
-    override fun getDisplayName(): String {
-        return "Unknown dependency: $description"
-    }
-}
