d7aab5eb6d4563fc1b50b8fb7709d48b82446921
Sergey Boishtyan <ssboishtyan@avito.ru>
2020-07-29 15:59:28 +0300

    MBS-8944 Disable inner step creation (#525)

--- subprojects/android-test/test-report/src/main/kotlin/com/avito/android/test/report/model/StepResult.kt
+++ subprojects/android-test/test-report/src/main/kotlin/com/avito/android/test/report/model/StepResult.kt
@@ -9,6 +9,14 @@ import com.avito.report.model.Entry
  * @param title in http://links.k.avito.ru/9O: "Step Action"; value must be the same (can be trimmed)
  */
 data class StepResult(
+    /**
+     * Synthetic step is created by the framework. It has simpilier lifecycle then Step created by users
+     * So we need that flag in the framework for making decisions:
+     * - What we should do when we start a new step or precondition and current step isn't NULL?
+     * If current step is Synthetic we will overwrite it.
+     * If current step is Real we will fail because we try to create INTERNAL step
+     */
+    val isSynthetic: Boolean,
     var timestamp: Long? = null,
     var number: Int? = null,
     var title: String? = null,
