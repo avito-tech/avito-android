aade5ca4a7f0741c88b1622655c610b249f0a319
Evgeniy Krivobokov <ekrivobokov@avito.ru>
2020-07-29 17:27:55 +0300

    MBS-9079 Fix logger backward compatibility

--- subprojects/common/logger/src/main/kotlin/com.avito.logger/Logger.kt
+++ subprojects/common/logger/src/main/kotlin/com.avito.logger/Logger.kt
@@ -2,7 +2,8 @@ package com.avito.logger
 
 interface Logger {
     fun debug(msg: String)
-    fun info(msg: String)
+    // TODO: implement in clients and remove empty implementation
+    fun info(msg: String) {} // empty implementation is for backward compatibility
     fun warn(msg: String, error: Throwable? = null)
     fun exception(msg: String, error: Throwable)
     fun critical(msg: String, error: Throwable)
