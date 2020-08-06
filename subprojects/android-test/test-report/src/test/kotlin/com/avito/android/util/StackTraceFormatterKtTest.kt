8d2695ef94552be50b36d8e6c24a2969dea9516b
Dmitriy Voronin <dvoronin@avito.ru>
2020-08-06 19:40:00 +0300

    MBS-9148 rename test

--- subprojects/android-test/test-report/src/test/kotlin/com/avito/android/util/StackTraceFormatterKtTest.kt
+++ subprojects/android-test/test-report/src/test/kotlin/com/avito/android/util/StackTraceFormatterKtTest.kt
@@ -6,13 +6,13 @@ import org.junit.jupiter.api.Test
 internal class StackTraceFormatterKtTest {
 
     @Test
-    fun test() {
+    fun `formatStackTrace result - contains first two lines of exception stacktrace`() {
         val exception = IllegalStateException("Something went wrong")
         val result: List<String> = exception.formatStackTrace()
 
         assertThat(result).containsAtLeast(
             "java.lang.IllegalStateException: Something went wrong",
-            "\tat com.avito.android.util.StackTraceFormatterKtTest.test(StackTraceFormatterKtTest.kt:10)"
+            "\tat com.avito.android.util.StackTraceFormatterKtTest.formatStackTrace result - contains first two lines of exception stacktrace(StackTraceFormatterKtTest.kt:10)"
         ).inOrder()
     }
 }
