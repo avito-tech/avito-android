c7d562e6abf86a5905c853c1f8346183c529379d
Daniil Popov <int02h@gmail.com>
2020-08-14 20:38:46 +0300

    MBS-9270 fix backward compatibility for Logger again (#547)

--- subprojects/common/logger/src/main/kotlin/com.avito.logger/Logger.kt
+++ subprojects/common/logger/src/main/kotlin/com.avito.logger/Logger.kt
@@ -5,4 +5,8 @@ interface Logger {
     fun info(msg: String)
     fun warn(msg: String, error: Throwable? = null)
     fun critical(msg: String, error: Throwable) {} // empty implementation is for backward compatibility
+
+    // TODO remove in MBS-9283
+    @Deprecated("Never called. Implement critical instead.")
+    fun exception(msg: String, error: Throwable) {} // empty implementation is for backward compatibility
 }
