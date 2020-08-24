f27a7fa8292c6917a3bcbc2e29613961efaea8eb
Dmitriy Voronin <dsvoronin@gmail.com>
2020-08-24 22:58:02 +0300

    MBS-9333 deprecate performance plugin (#560)
    we decided to stop investing in blocking performance tests for now

--- subprojects/gradle/performance/src/main/kotlin/com/avito/performance/PerformanceCompareTask.kt
+++ subprojects/gradle/performance/src/main/kotlin/com/avito/performance/PerformanceCompareTask.kt
@@ -1,78 +1,12 @@
 package com.avito.performance
 
-import com.avito.android.stats.statsdConfig
-import com.avito.bitbucket.atlassianCredentials
-import com.avito.bitbucket.bitbucketConfig
-import com.avito.bitbucket.pullRequestId
-import com.avito.kotlin.dsl.getBooleanProperty
-import com.avito.utils.gradle.envArgs
-import com.avito.utils.logging.ciLogger
 import org.gradle.api.DefaultTask
-import org.gradle.api.file.RegularFileProperty
-import org.gradle.api.model.ObjectFactory
-import org.gradle.api.tasks.InputFile
-import org.gradle.api.tasks.Internal
-import org.gradle.api.tasks.Optional
-import org.gradle.api.tasks.OutputFile
 import org.gradle.api.tasks.TaskAction
-import org.gradle.kotlin.dsl.property
-import org.gradle.workers.IsolationMode
-import org.gradle.workers.WorkerExecutor
-import javax.inject.Inject
 
-@Suppress("UnstableApiUsage")
-abstract class PerformanceCompareTask @Inject constructor(
-    objects: ObjectFactory,
-    private val workerExecutor: WorkerExecutor
-) : DefaultTask() {
-
-    @Optional
-    @InputFile
-    val targetResults: RegularFileProperty = objects.fileProperty()
-
-    //todo support @Optional
-    @InputFile
-    val sourceResults: RegularFileProperty = objects.fileProperty()
-
-    @Internal
-    val reportApiUrl = objects.property<String>()
-
-    @Internal
-    val reportApiFallbackUrl = objects.property<String>()
-
-    @Internal
-    val statsUrl = objects.property<String>()
-
-    @OutputFile
-    val comparison: RegularFileProperty = objects.fileProperty()
+abstract class PerformanceCompareTask : DefaultTask() {
 
     @TaskAction
     fun action() {
-
-        //todo новое api, когда выйдет в stable
-        // https://docs.gradle.org/5.6/userguide/custom_tasks.html#using-the-worker-api
-        @Suppress("DEPRECATION")
-        workerExecutor.submit(PerformanceCompareAction::class.java) { workerConfiguration ->
-            workerConfiguration.isolationMode = IsolationMode.NONE
-            workerConfiguration.setParams(
-                PerformanceCompareAction.Params(
-                    previousTests = targetResults.orNull?.asFile,
-                    currentTests = sourceResults.asFile.get(),
-                    comparison = comparison.asFile.get(),
-                    buildUrl = project.envArgs.build.url,
-                    logger = ciLogger,
-                    reportApiUrl = reportApiUrl.get(),
-                    reportApiFallbackUrl = reportApiFallbackUrl.get(),
-                    atlassianCredentials = project.atlassianCredentials.get(),
-                    pullRequestId = project.pullRequestId.orNull,
-                    enablePrPerformanceReporting = project.getBooleanProperty("enablePrPerformanceReporting"),
-                    bitbucketConfig = project.bitbucketConfig.get(),
-                    statsdConfig = project.statsdConfig.get(),
-                    //todo handle not defined
-                    slackConfig = project.slackConfig.get(),
-                    statsUrl = statsUrl.get()
-                )
-            )
-        }
+        logger.lifecycle("Performance plugin is deprecated. If you see this message, consider removing performance plugin")
     }
 }
--- subprojects/gradle/performance/src/main/kotlin/com/avito/performance/PerformancePlugin.kt
+++ subprojects/gradle/performance/src/main/kotlin/com/avito/performance/PerformancePlugin.kt
@@ -1,92 +1,36 @@
 package com.avito.performance
 
-import com.avito.git.gitState
-import com.avito.git.isOnDefaultBranch
-import com.avito.instrumentation.InstrumentationTestsPlugin
 import com.avito.instrumentation.configuration.InstrumentationConfiguration
 import com.avito.instrumentation.instrumentationTask
 import com.avito.instrumentation.withInstrumentationTests
-import com.avito.kotlin.dsl.dependencyOn
-import com.avito.kotlin.dsl.toOptional
-import com.avito.report.model.RunId
-import com.avito.utils.gradle.envArgs
-import com.avito.utils.logging.ciLogger
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.Task
-import org.gradle.api.internal.provider.Providers
-import org.gradle.api.provider.Provider
 import org.gradle.kotlin.dsl.create
 import org.gradle.kotlin.dsl.register
-import java.io.File
 
-@Suppress("UnstableApiUsage")
 open class PerformancePlugin : Plugin<Project> {
 
     override fun apply(project: Project) {
 
-        require(project.plugins.hasPlugin(InstrumentationTestsPlugin::class.java)) {
-            "PerformancePlugin works only with applied ${InstrumentationTestsPlugin::class.java}"
-        }
-
-        val extension = project.extensions.create<PerformanceExtension>("performance")
-
-        val logger = project.ciLogger
-
-        val gitState = project.gitState { logger.info(it) }
-
-        val envArgs = project.envArgs
-
-        val graphiteKeyProvider = gitState.map {
-            if (it.isOnDefaultBranch) {
-                it.defaultBranch
-            } else {
-                "not-defined"
-            }
-        }.orElse("not-defined")
+        project.extensions.create<PerformanceExtension>("performance")
 
         project.withInstrumentationTests { instrumentationConfig ->
-            // Apply performance only for Instrumentation with reportViewerConfig
-            val reportViewerConfig = requireNotNull(instrumentationConfig.reportViewer) {
-                "PerformancePlugin could be applied only with reportViewer report"
-            }
-            val performanceOutputDir = extension.output.get()
-            val performanceResultsFileName = extension.performanceTestResultName.get()
-
             instrumentationConfig.configurations
                 .filter { it.performanceType == InstrumentationConfiguration.PerformanceType.MDE }
                 .forEach { performanceMdeConfig ->
-
-                    logger.debug("Creating performance mde configuration: $performanceMdeConfig")
-
                     project.tasks.instrumentationTask(performanceMdeConfig.name) { instrumentationTask ->
 
-                        val performanceMdeCollectProvider =
-                            project.tasks.register<PerformanceCollectTask>("collect${performanceMdeConfig.name.capitalize()}") {
-                                group = TASK_GROUP
-                                description = "Collect performance data nightly"
-                                graphiteKey.set(graphiteKeyProvider)
-                                performanceTests.set(
-                                    File(
-                                        performanceOutputDir,
-                                        "${performanceMdeConfig.name}_${performanceResultsFileName}"
-                                    )
-                                )
-                                buildId.set(envArgs.build.id.toString())
-                                reportApiUrl.set(reportViewerConfig.reportApiUrl)
-                                reportApiFallbackUrl.set(reportViewerConfig.reportApiFallbackUrl)
-                                reportCoordinates.set(performanceMdeConfig.instrumentationParams.reportCoordinates())
-                                dependsOn(instrumentationTask)
-                            }
+                        project.tasks.register<PerformanceCollectTask>("collect${performanceMdeConfig.name.capitalize()}") {
+                            group = TASK_GROUP
+                            description = "Collect performance data nightly"
+
+                            dependsOn(instrumentationTask)
+                        }
 
                         project.tasks.register<SendPerformanceMdeTask>("sendPerformanceMdeTask") {
                             group = TASK_GROUP
                             description = "Report for mde calculations"
-                            statsUrl.set(extension.statsUrl)
-
-                            dependencyOn(performanceMdeCollectProvider) { mdeCollectTask ->
-                                currentTests.set(mdeCollectTask.performanceTests)
-                            }
                         }
                     }
                 }
@@ -95,105 +39,32 @@ open class PerformancePlugin : Plugin<Project> {
                 .filter { it.performanceType == InstrumentationConfiguration.PerformanceType.SIMPLE }
                 .forEach { performanceConfig ->
 
-                    logger.debug("Creating performance configuration: $performanceConfig")
-
-                    val reportCoordinates = performanceConfig.instrumentationParams.reportCoordinates()
-
                     project.tasks.instrumentationTask(performanceConfig.name) { instrumentationTask ->
-
-                        logger.debug("Based on instrumentation task: $instrumentationTask")
 
                         val sourceBranchResultsCollector =
                             project.tasks.register<PerformanceCollectTask>("collect${performanceConfig.name.capitalize()}") {
                                 group = TASK_GROUP
                                 description = "Collect performance data"
-                                graphiteKey.set(graphiteKeyProvider)
-                                performanceTests.set(
-                                    File(
-                                        performanceOutputDir,
-                                        "${performanceConfig.name}_${performanceResultsFileName}"
-                                    )
-                                )
-                                this.reportCoordinates.set(reportCoordinates)
-                                buildId.set(envArgs.build.id.toString())
-                                reportApiUrl.set(reportViewerConfig.reportApiUrl)
-                                reportApiFallbackUrl.set(reportViewerConfig.reportApiFallbackUrl)
+
                                 dependsOn(instrumentationTask)
                             }
 
-                        val targetBranchResultsCollector =
-                            project.tasks.register<PerformanceCollectTask>("download${performanceConfig.name.capitalize()}") {
-                                group = TASK_GROUP
-                                description = "Download performance report from target branch build"
-                                graphiteKey.set(
-                                    gitState.map {
-                                        // fail fast for more meaningful error message
-                                        requireNotNull(it.targetBranch?.name)
-                                        { "Can't run performance tasks without targetBranch specified" }
-                                    })
-                                performanceTests.set(
-                                    File(
-                                        performanceOutputDir,
-                                        "previous_${performanceConfig.name}_${performanceResultsFileName}"
-                                    )
-                                )
-                                buildId.set(envArgs.build.id.toString())
-                                reportApiUrl.set(reportViewerConfig.reportApiUrl)
-                                reportApiFallbackUrl.set(reportViewerConfig.reportApiFallbackUrl)
+                        project.tasks.register<PerformanceCollectTask>("download${performanceConfig.name.capitalize()}") {
+                            group = TASK_GROUP
+                            description = "Download performance report from target branch build"
 
-                                //there is no need to depend on tests, it is a hack to postpone fetching
-                                dependsOn(sourceBranchResultsCollector)
-
-                                //todo probably it's a mistake; but we need to fetch teamcity API otherwise
-                                buildId.set(null as String?)
-
-                                this.reportCoordinates.set(
-                                    reportCoordinates.copy(
-                                        runId = RunId(
-                                            commitHash = gitState.map { it.targetBranch!!.commit }.get(),
-                                            buildTypeId = extension.targetBuildConfigId.get()
-                                        ).toString()
-                                    )
-                                )
-                            }
-
-                        val performanceCompareProvider =
-                            project.tasks.register<PerformanceCompareTask>("compare${performanceConfig.name.capitalize()}") {
-                                group = TASK_GROUP
-                                description = "Compare performance reports"
-                                comparison.set(File(performanceOutputDir, "comparison.json"))
-                                reportApiUrl.set(reportViewerConfig.reportApiUrl)
-                                reportApiFallbackUrl.set(reportViewerConfig.reportApiFallbackUrl)
-                                statsUrl.set(extension.statsUrl)
-
-                                dependencyOn(sourceBranchResultsCollector) { sourceTestsCollector ->
-                                    sourceResults.set(sourceTestsCollector.performanceTests)
-                                }
-
-                                dependencyOn(targetBranchResultsCollector) { targetTestsCollector ->
-                                    targetResults.set(targetTestsCollector.performanceTests.toOptional())
-                                }
-                            }
-
-                        val performanceProvider: Provider<Task> = gitState.flatMap {
-                            when {
-                                it.isOnDefaultBranch -> sourceBranchResultsCollector
-                                it.targetBranch != null -> performanceCompareProvider
-                                else -> Providers.notDefined<Task>()
-                            }
+                            //there is no need to depend on tests, it is a hack to postpone fetching
+                            dependsOn(sourceBranchResultsCollector)
                         }
 
-                        logger.debug("performanceProvider=${performanceProvider.orNull}")
-                        logger.debug("performanceProvider=${performanceProvider.isPresent}")
+                        project.tasks.register<PerformanceCompareTask>("compare${performanceConfig.name.capitalize()}") {
+                            group = TASK_GROUP
+                            description = "Compare performance reports"
+                        }
 
                         project.tasks.register<Task>(measurePerformanceTaskName(performanceConfig.name)) {
                             this.group = TASK_GROUP
                             this.description = "Measure performance"
-
-                            if (performanceProvider.isPresent) {
-                                this.dependsOn(performanceProvider)
-                            }
-                            this.onlyIf { performanceProvider.isPresent }
                         }
                     }
                 }
