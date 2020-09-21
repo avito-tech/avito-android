17a3c33adb36f04cb7a550b27304e8b57f69b822
Dmitriy Voronin <dsvoronin@gmail.com>
2020-09-21 15:46:55 +0300

    MBS-9446 fix concurrent access in MockDispatcher (#577)

--- subprojects/common/test-okhttp/src/test/kotlin/com/avito/test/http/MockDispatcherTest.kt
+++ subprojects/common/test-okhttp/src/test/kotlin/com/avito/test/http/MockDispatcherTest.kt
@@ -5,13 +5,31 @@ import okhttp3.Headers
 import okhttp3.mockwebserver.MockResponse
 import okhttp3.mockwebserver.RecordedRequest
 import okio.Buffer
+import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
+import java.io.OutputStream
+import java.io.PrintStream
 import java.net.InetAddress
 import java.nio.charset.Charset
 
 internal class MockDispatcherTest {
 
-    private val dispatcher = MockDispatcher(logger = { println(it) })
+    //todo create TestLogger for it
+    private val devNull = PrintStream(object : OutputStream() {
+        override fun write(b: Int) {
+        }
+    })
+
+    private val isLocalRun: Boolean by lazy {
+        val command: String? = System.getProperty("sun.java.command")
+        command?.contains("intellij") ?: false
+    }
+
+    private val output = if (isLocalRun) System.out else devNull
+
+    private val logger: (String) -> Unit = { output.println(it) }
+
+    private val dispatcher = MockDispatcher(logger = logger)
 
     @Test
     fun `dispatcher - dispatch last matching response - if multiple registered conditions matches`() {
@@ -53,6 +71,44 @@ internal class MockDispatcherTest {
         val response = dispatcher.dispatch(buildRequest(path = "xxx", body = "param1485"))
 
         assertThat(response.getBody()?.readUtf8()).isEqualTo("First registered")
+    }
+
+    /**
+     * Run with "Repeat: Until failure" option in IDEA run configuration
+     * see MBS-7636
+     */
+    @Disabled("Used only for manual debugging of concurrency problems on dispatcher internal collections")
+    @Test
+    fun `dispatcher - register and dispatching mocks - accessed concurrently`() {
+        var throwable: Throwable? = null
+
+        val registers = Thread {
+            repeat(100) {
+                try {
+                    dispatcher.registerMock(Mock({ true }, MockResponse(), true))
+                } catch (e: Throwable) {
+                    throwable = e
+                }
+            }
+        }
+
+        val dispatchers = Thread {
+            repeat(100) {
+                try {
+                    dispatcher.dispatch(buildRequest())
+                } catch (e: Throwable) {
+                    throwable = e
+                }
+            }
+        }
+
+        registers.start()
+        dispatchers.start()
+
+        registers.join()
+        dispatchers.join()
+
+        assertThat(throwable).isNull()
     }
 }
 
