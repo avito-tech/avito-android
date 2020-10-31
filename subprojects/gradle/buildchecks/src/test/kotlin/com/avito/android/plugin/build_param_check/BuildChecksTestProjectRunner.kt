73ed5ddb5a9a846bbb740482f230fb2c4f95b9e3
Eugene Krivobokov <eugene.krivobokov@gmail.com>
2020-10-31 23:07:08 +0300

    Fix build check plugin in a custom project directory (#614)

--- subprojects/gradle/buildchecks/src/test/kotlin/com/avito/android/plugin/build_param_check/BuildChecksTestProjectRunner.kt
+++ subprojects/gradle/buildchecks/src/test/kotlin/com/avito/android/plugin/build_param_check/BuildChecksTestProjectRunner.kt
@@ -4,6 +4,7 @@ import com.avito.test.gradle.TestProjectGenerator
 import com.avito.test.gradle.TestResult
 import com.avito.test.gradle.file
 import com.avito.test.gradle.gradlew
+import com.avito.test.gradle.module.AndroidAppModule
 import java.io.File
 
 class BuildChecksTestProjectRunner(
@@ -18,7 +19,14 @@ class BuildChecksTestProjectRunner(
         class Custom(val dir: File) : AndroidHomeLocation()
     }
 
-    fun runChecks(expectFailure: Boolean = false): TestResult {
+    /***
+     * @param startDir the start directory for Gradle (--project-dir). Defaults to the root project directory.
+     */
+    fun runChecks(
+        expectFailure: Boolean = false,
+        disablePlugin: Boolean = false,
+        startDir: String? = null
+    ): TestResult {
         val androidHomePath = if (androidHome is AndroidHomeLocation.Custom) androidHome.dir.path else null
 
         val environment: Map<String, String>? = when (androidHome) {
@@ -28,7 +36,9 @@ class BuildChecksTestProjectRunner(
         }
         TestProjectGenerator(
             plugins = listOf("com.avito.android.buildchecks"),
-            modules = emptyList(),
+            modules = listOf(
+                AndroidAppModule("app")
+            ),
             buildGradleExtra = """
                 buildChecks {
                     $buildChecksExtension
@@ -41,15 +51,22 @@ class BuildChecksTestProjectRunner(
             // remove TestProjectGenerator fallback
             projectDir.file("local.properties").delete()
         }
-
-        return gradlew(
-            projectDir,
+        val arguments = mutableListOf(
             "help",
             //todo make params optional
             "-Pavito.stats.host=localhost",
             "-Pavito.stats.fallbackHost=localhost",
             "-Pavito.stats.port=80",
-            "-Pavito.stats.namespace=stub",
+            "-Pavito.stats.namespace=stub"
+        )
+        if (disablePlugin) {
+            arguments.add("-Pavito.build-checks.enabled=false")
+        }
+        val dir = if (startDir == null) projectDir else File(projectDir, startDir)
+
+        return gradlew(
+            dir,
+            *arguments.toTypedArray(),
             expectFailure = expectFailure,
             environment = environment
         )
