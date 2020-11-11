bd754b8650a1c7a7487408e1491b47e8e34e84d9
Dmitriy Voronin <dvoronin@avito.ru>
2020-11-11 12:53:46 +0300

    version bump and cherry picks from release branch with fixes

--- subprojects/gradle/lint-report/src/main/kotlin/com/avito/android/lint/AndroidLintAccessor.kt
+++ subprojects/gradle/lint-report/src/main/kotlin/com/avito/android/lint/AndroidLintAccessor.kt
@@ -2,6 +2,7 @@ package com.avito.android.lint
 
 import org.gradle.api.Project
 import org.gradle.api.Task
+import org.gradle.api.UnknownTaskException
 import org.gradle.api.tasks.TaskProvider
 import java.io.File
 
@@ -12,7 +13,12 @@ import java.io.File
  */
 class AndroidLintAccessor(private val project: Project) {
 
-    fun taskProvider(): TaskProvider<Task> = project.tasks.named("lintRelease")
+    fun taskProvider(): TaskProvider<Task> = try {
+        project.tasks.named("lintRelease")
+    } catch (e: UnknownTaskException) {
+        // local android sync usually doesn't have release variant enabled
+        project.tasks.named("lint")
+    }
 
     fun resultXml(): File = File("${project.buildDir}/reports/lint-results-release.xml")
 
