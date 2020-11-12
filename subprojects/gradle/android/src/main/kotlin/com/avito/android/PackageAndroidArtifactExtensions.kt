2f829bd94d29ce2c42108c4d52d924671ab68634
Evgeniy Krivobokov <ekrivobokov@avito.ru>
2020-11-12 20:41:16 +0300

    Basic support AGP 4.1 in instrumentation plugin

--- subprojects/gradle/android/src/main/kotlin/com/avito/android/PackageAndroidArtifactExtensions.kt
+++ subprojects/gradle/android/src/main/kotlin/com/avito/android/PackageAndroidArtifactExtensions.kt
@@ -1,10 +1,27 @@
 package com.avito.android
 
 import com.android.build.gradle.tasks.PackageAndroidArtifact
+import org.gradle.api.file.Directory
+import org.gradle.api.file.DirectoryProperty
 import java.io.File
 
-fun PackageAndroidArtifact.getApkFile(): File {
-    val outputDir = outputDirectory.get().asFile
+// TODO: Use Artifacts API
+fun PackageAndroidArtifact.apkDirectory(): DirectoryProperty = outputDirectory
 
-    return File(outputDir, TODO())
+fun Directory.getApk(): File? {
+    val apks = asFile.listFiles()
+        .filter {
+            it.extension == "apk"
+        }
+
+    require(apks.size < 2) {
+        "Multiple APK are not supported. Dir: ${asFile}"
+    }
+    return apks.firstOrNull()
+}
+
+fun Directory.getApkOrThrow(): File {
+    return requireNotNull(getApk()) {
+        "APK not found in ${asFile}. Files in dir: ${asFile.listFiles().joinToString()}"
+    }
 }
