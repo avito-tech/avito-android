660cb976ddb339f984953023b0c85f38b51daefc
Dmitriy Voronin <dvoronin@avito.ru>
2020-11-20 03:05:10 +0300

    detekt: enable and fix MatchingDeclarationName rule
    explain why InvalidPackageDeclaration blocked

--- subprojects/gradle/lint-report/src/main/kotlin/com/avito/android/lint/html/HtmlExtensions.kt
+++ subprojects/gradle/lint-report/src/main/kotlin/com/avito/android/lint/html/HtmlExtensions.kt
@@ -7,11 +7,10 @@ import kotlinx.html.TagConsumer
 import kotlinx.html.attributesMapOf
 import kotlinx.html.visit
 
-internal open class MAIN(initialAttributes: Map<String, String>, override val consumer: TagConsumer<*>) :
-    HTMLTag("main", consumer, initialAttributes, null, false, false),
-    CommonAttributeGroupFacadeFlowSectioningContent
-
 internal fun SectioningOrFlowContent.main(classes: String? = null, block: MAIN.() -> Unit = {}): Unit = MAIN(
     attributesMapOf("class", classes), consumer
 ).visit(block)
 
+internal open class MAIN(initialAttributes: Map<String, String>, override val consumer: TagConsumer<*>) :
+    HTMLTag("main", consumer, initialAttributes, null, false, false),
+    CommonAttributeGroupFacadeFlowSectioningContent
