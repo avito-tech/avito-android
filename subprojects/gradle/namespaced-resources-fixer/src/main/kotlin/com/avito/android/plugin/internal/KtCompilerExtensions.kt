2bae4fe6690a279ce485b7a1bb1105e4075f3f47
Evgeniy Krivobokov <ekrivobokov@avito.ru>
2020-11-27 13:43:27 +0300

    MBS-9985 Remove unnecessary experimental plugin

--- subprojects/gradle/namespaced-resources-fixer/src/main/kotlin/com/avito/android/plugin/internal/KtCompilerExtensions.kt
+++
@@ -1,7 +0,0 @@
-package com.avito.android.plugin.internal
-
-import io.github.detekt.parser.KtCompiler
-import org.jetbrains.kotlin.psi.KtFile
-import java.io.File
-
-internal fun KtCompiler.parse(file: File): KtFile = compile(file.toPath(), file.toPath())
