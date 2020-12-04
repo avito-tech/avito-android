de4ed17ee49f3ec7c58b96bda39a02e0555f63a6
Dmitriy Voronin <dvoronin@avito.ru>
2020-12-04 20:34:43 +0300

    MBS-9457 androidTestDir to @Input; remove useless extension

--- subprojects/gradle/instrumentation-changed-tests-finder/src/main/kotlin/com/avito/android/FindChangedTestsTask.kt
+++ subprojects/gradle/instrumentation-changed-tests-finder/src/main/kotlin/com/avito/android/FindChangedTestsTask.kt
@@ -7,7 +7,9 @@ import com.avito.instrumentation.impact.KotlinClassesFinderImpl
 import com.avito.utils.logging.ciLogger
 import com.avito.utils.rewriteNewLineList
 import org.gradle.api.DefaultTask
+import org.gradle.api.file.ProjectLayout
 import org.gradle.api.file.RegularFile
+import org.gradle.api.file.RegularFileProperty
 import org.gradle.api.model.ObjectFactory
 import org.gradle.api.provider.Provider
 import org.gradle.api.tasks.Input
@@ -18,20 +20,26 @@ import org.gradle.kotlin.dsl.property
 import java.io.File
 import javax.inject.Inject
 
+@Suppress("UnstableApiUsage")
 class FindChangedTestsTask @Inject constructor(
-    extension: ModifiedTestsFinderExtension,
-    objects: ObjectFactory
+    objects: ObjectFactory,
+    layout: ProjectLayout
 ) : DefaultTask() {
 
     @Input
     val targetCommit = objects.property<String>()
 
-    @Suppress("UnstableApiUsage")
     @InputFile
-    val allTestsInApk = objects.fileProperty()
+    val androidTestDir: RegularFileProperty = objects.fileProperty()
+        .convention { File(project.projectDir, "src/androidTest") }
+
+    @InputFile
+    val allTestsInApk: RegularFileProperty = objects.fileProperty()
 
     @OutputFile
-    val modifiedTestsFile: Provider<RegularFile> = extension.output.file("changed-tests.txt")
+    val modifiedTestsFile: Provider<RegularFile> = objects.directoryProperty()
+        .convention(layout.buildDirectory)
+        .file("changed-tests.txt")
 
     @TaskAction
     fun doWork() {
@@ -48,10 +56,8 @@ class FindChangedTestsTask @Inject constructor(
             kotlinClassesFinder = KotlinClassesFinderImpl()
         )
 
-        val androidTestDir = File(project.projectDir, "src/androidTest")
-
         val modifiedTestNames = action.find(
-            androidTestDir = androidTestDir,
+            androidTestDir = androidTestDir.get().asFile,
             allTestsInApk = allTestsInApk.readTestsInApk().map { it.testName }
         ).get()
 
--- subprojects/gradle/instrumentation-changed-tests-finder/src/main/kotlin/com/avito/android/InstrumentationChangedTestsFinderPlugin.kt
+++ subprojects/gradle/instrumentation-changed-tests-finder/src/main/kotlin/com/avito/android/InstrumentationChangedTestsFinderPlugin.kt
@@ -2,14 +2,11 @@ package com.avito.android
 
 import org.gradle.api.Plugin
 import org.gradle.api.Project
-import org.gradle.kotlin.dsl.create
 import org.gradle.kotlin.dsl.register
 
 class InstrumentationChangedTestsFinderPlugin : Plugin<Project> {
 
     override fun apply(target: Project) {
-        target.extensions.create<ModifiedTestsFinderExtension>("modifiedTestsFinder")
-
         target.tasks.register<FindChangedTestsTask>(changedTestsFinderTaskName) {
             group = "impact-analysis"
         }
