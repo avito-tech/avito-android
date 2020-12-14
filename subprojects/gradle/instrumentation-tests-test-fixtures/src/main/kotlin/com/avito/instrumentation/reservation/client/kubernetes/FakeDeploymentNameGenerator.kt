549b1f38f26addd4fe08e7f07f22f83aa2be16ed
Dmitriy Voronin <dvoronin@avito.ru>
2020-12-14 18:45:18 +0300

    rename FakeDeploymentNameGenerator param

--- subprojects/gradle/instrumentation-tests-test-fixtures/src/main/kotlin/com/avito/instrumentation/reservation/client/kubernetes/FakeDeploymentNameGenerator.kt
+++ subprojects/gradle/instrumentation-tests-test-fixtures/src/main/kotlin/com/avito/instrumentation/reservation/client/kubernetes/FakeDeploymentNameGenerator.kt
@@ -2,10 +2,14 @@ package com.avito.instrumentation.reservation.client.kubernetes
 
 import com.avito.utils.gradle.toValidKubernetesName
 
+/**
+ * @param postfix stable postfix of deployment name for testing (production variant contains random sequence)
+ *   see [UUIDDeploymentNameGenerator]
+ */
 class FakeDeploymentNameGenerator(
-    private val variablePart: String = "integration-test"
+    private val postfix: String = "integration-test"
 ) : DeploymentNameGenerator {
 
     override fun generateName(namespace: String): String =
-        "$namespace-$variablePart".toValidKubernetesName()
+        "$namespace-$postfix".toValidKubernetesName()
 }
