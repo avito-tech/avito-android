d8b25d249af19d0597e5cc13c4db650ef65cbd33
Dmitriy Voronin <dvoronin@avito.ru>
2020-12-15 18:54:45 +0300

    remove useless comment

--- subprojects/gradle/kubernetes/src/main/kotlin/com/avito/utils/gradle/Naming.kt
+++ subprojects/gradle/kubernetes/src/main/kotlin/com/avito/utils/gradle/Naming.kt
@@ -1,8 +1,6 @@
 package com.avito.utils.gradle
 
 /**
- * if you encounter any problems with generated named, feel free to add more strict checks and changes
- *
  * Refer to official rules: https://kubernetes.io/docs/concepts/overview/working-with-objects/names/
  */
 fun String.toValidKubernetesName(): String = replace("_", "-").toLowerCase()
