da7ceba90f264ff8ed03ef971b0712cfa9e8ca7d
Eugene Krivobokov <eugene.krivobokov@gmail.com>
2020-12-16 18:25:43 +0300

    Fix CILogger serialization (#678)

---
+++ subprojects/common/elastic/src/main/kotlin/com/avito/android/elastic/ElasticFactory.kt
@@ -0,0 +1,31 @@
+package com.avito.android.elastic
+
+import com.avito.time.DefaultTimeProvider
+import okhttp3.OkHttpClient
+import java.util.concurrent.TimeUnit
+
+object ElasticFactory {
+
+    private const val defaultTimeoutSec = 10L
+
+    fun create(config: ElasticConfig, onError: (String, Throwable?) -> Unit): ElasticClient {
+        return when (config) {
+            is ElasticConfig.Disabled -> StubElasticClient
+            is ElasticConfig.Enabled -> {
+                val okHttpClient = OkHttpClient.Builder()
+                    .connectTimeout(defaultTimeoutSec, TimeUnit.SECONDS)
+                    .readTimeout(defaultTimeoutSec, TimeUnit.SECONDS)
+                    .build()
+
+                HttpElasticClient(
+                    okHttpClient = okHttpClient,
+                    timeProvider = DefaultTimeProvider(),
+                    endpoint = config.endpoint,
+                    indexPattern = config.indexPattern,
+                    buildId = config.buildId,
+                    onError = onError
+                )
+            }
+        }
+    }
+}
