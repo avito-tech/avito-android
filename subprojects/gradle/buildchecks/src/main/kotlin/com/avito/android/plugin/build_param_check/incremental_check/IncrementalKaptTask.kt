bf3bf8e1391d93e6597ff9bbc79fda1695247b1c
Dmitriy Voronin <dsvoronin@gmail.com>
2021-01-25 18:38:13 +0300

    fix accessing system properties and it's nullability (#731)
    * fix accessing system properties and it's nullability
    
    * rename to BuildEnvironmentInfo

--- subprojects/gradle/buildchecks/src/main/kotlin/com/avito/android/plugin/build_param_check/incremental_check/IncrementalKaptTask.kt
+++ subprojects/gradle/buildchecks/src/main/kotlin/com/avito/android/plugin/build_param_check/incremental_check/IncrementalKaptTask.kt
@@ -1,5 +1,6 @@
 package com.avito.android.plugin.build_param_check.incremental_check
 
+import com.avito.android.plugin.build_param_check.BuildEnvironmentInfo
 import com.avito.android.plugin.build_param_check.CheckResult
 import com.avito.android.plugin.build_param_check.CheckTaskWithMode
 import com.avito.kotlin.dsl.getBooleanProperty
@@ -8,9 +9,13 @@ import com.avito.logger.LoggerFactory
 import com.avito.utils.BuildFailer
 import com.avito.utils.buildFailer
 import org.gradle.api.Project
+import org.gradle.api.model.ObjectFactory
+import org.gradle.api.tasks.Internal
 import org.gradle.api.tasks.TaskAction
+import org.gradle.kotlin.dsl.property
+import javax.inject.Inject
 
-internal abstract class IncrementalKaptTask : CheckTaskWithMode() {
+internal abstract class IncrementalKaptTask @Inject constructor(objects: ObjectFactory) : CheckTaskWithMode() {
 
     private val Project.hasKotlinKapt: Boolean
         get() = plugins.hasPlugin("kotlin-kapt")
@@ -20,17 +25,28 @@ internal abstract class IncrementalKaptTask : CheckTaskWithMode() {
             it.group == "androidx.room" && it.name == "room-compiler"
         } ?: false
 
+    @get:Internal
+    val accessor = objects.property<BuildEnvironmentInfo>()
+
     @TaskAction
     fun check() {
         val buildFailer = project.buildFailer
         val loggerFactory = GradleLoggerFactory.fromTask(this)
         val incrementalKaptEnabled = project.getBooleanProperty("kapt.incremental.apt", default = false)
         if (incrementalKaptEnabled) {
-            checkAnnotationProcessors(buildFailer, loggerFactory)
+            checkAnnotationProcessors(
+                failer = buildFailer,
+                loggerFactory = loggerFactory,
+                envInfo = accessor.get()
+            )
         }
     }
 
-    private fun checkAnnotationProcessors(failer: BuildFailer, loggerFactory: LoggerFactory) {
+    private fun checkAnnotationProcessors(
+        failer: BuildFailer,
+        loggerFactory: LoggerFactory,
+        envInfo: BuildEnvironmentInfo
+    ) {
         val subProject = project.subprojects.firstOrNull {
             it.hasKotlinKapt && it.hasRoomKapt
         }
@@ -39,16 +55,16 @@ internal abstract class IncrementalKaptTask : CheckTaskWithMode() {
                 if (RoomIncrementalKaptChecker(subProject, loggerFactory = loggerFactory).isSupported()) {
                     CheckResult.Ok
                 } else {
-                    CheckResult.Failed(collectErrorMessage())
+                    CheckResult.Failed(collectErrorMessage(envInfo))
                 }
             }
         }
     }
 
-    private fun collectErrorMessage() = """
+    private fun collectErrorMessage(envInfo: BuildEnvironmentInfo) = """
         Incremental KAPT is turned on (kapt.incremental.apt=true) but Room does not support it in current conditions. 
         You have to use JDK 11 and higher or embedded one in Android Studio 3.5.0-beta02 and higher.
-        Current JDK is ${System.getProperty("java.runtime.version")} provided by ${System.getProperty("java.vendor")}.
+        Current JDK is ${envInfo.javaInfo}.
         https://avito-tech.github.io/avito-android/docs/projects/buildchecks/#room
     """.trimIndent()
 }
