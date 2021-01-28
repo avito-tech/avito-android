1fb6eadc5b384240d7e268da5fe096c986196e12
Dmitriy Voronin <dvoronin@avito.ru>
2021-01-29 00:46:49 +0300

    make timestamps nullable

--- subprojects/gradle/runner/client/src/main/kotlin/com/avito/runner/scheduler/metrics/model/DeviceTimestamps.kt
+++ subprojects/gradle/runner/client/src/main/kotlin/com/avito/runner/scheduler/metrics/model/DeviceTimestamps.kt
@@ -3,22 +3,29 @@ package com.avito.runner.scheduler.metrics.model
 import com.avito.math.percentOf
 
 internal data class DeviceTimestamps(
-    val created: Long,
+    val created: Long? = null,
     val testTimestamps: MutableMap<TestKey, TestTimestamps>,
-    val finished: Long
+    val finished: Long? = null
 ) {
 
-    private val totalTime
-        get() = finished - created
+    private val totalTime: Long?
+        get() = if (finished != null && created != null) {
+            finished - created
+        } else {
+            null
+        }
 
     private val effectiveWorkTime
         get() = testTimestamps.values.mapNotNull { it.effectiveWorkTime }.sum()
 
     val utilizationPercent: Int?
-        get() = if (totalTime > effectiveWorkTime) {
-            effectiveWorkTime.percentOf(totalTime).toInt()
-        } else {
-            null // in case if device died and finish time not logged
+        get() {
+            val localTotalTime = totalTime
+            return if (localTotalTime != null) {
+                effectiveWorkTime.percentOf(localTotalTime).toInt()
+            } else {
+                null // in case if device died and finish time not logged
+            }
         }
 
     companion object
