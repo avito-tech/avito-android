3173af7e1a3da186d3b1ed2bbfbaf648ee195e23
Dmitriy Voronin <dsvoronin@gmail.com>
2021-01-28 13:44:54 +0300

    add logger kdoc (#739)

--- subprojects/common/logger/src/main/kotlin/com/avito/logger/Logger.kt
+++ subprojects/common/logger/src/main/kotlin/com/avito/logger/Logger.kt
@@ -1,8 +1,27 @@
 package com.avito.logger
 
+/**
+ * discussion about log levels https://github.com/avito-tech/avito-android/discussions/698
+ */
 interface Logger {
+
+    /**
+     * additional information for troubleshooting
+     */
     fun debug(msg: String)
+
+    /**
+     * to understand system state
+     */
     fun info(msg: String)
+
+    /**
+     * unwanted state but can proceed
+     */
     fun warn(msg: String, error: Throwable? = null)
+
+    /**
+     * could not recover from this state
+     */
     fun critical(msg: String, error: Throwable)
 }
