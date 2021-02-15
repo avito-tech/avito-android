d7d248af367a75aa5863835478ae9ece300f5254
Dmitriy Voronin <dsvoronin@gmail.com>
2021-02-15 13:21:48 +0300

    add GRADLE_USER_HOME env, fixes cache and wrapper volumes mounts (#782)
    * add GRADLE_USER_HOME env, fixes cache and wrapper volumes mounts
    
    * update image with changed entrypoint addressing missing GRADLE_USER_HOME

--- ci/docker/android-builder/entrypoint.sh
+++ ci/docker/android-builder/entrypoint.sh
@@ -6,20 +6,22 @@ set -e
 
 USER_ID=${LOCAL_USER_ID:-9001}
 BUILD_USER=build_user
-GRADLE_USER_HOME=/gradle
 
 echo "Starting with UID : $USER_ID"
 groupadd --gid "${USER_ID}" ${BUILD_USER}
 useradd --shell /bin/bash --uid "${USER_ID}" --gid "${USER_ID}" --comment "User for container" --create-home ${BUILD_USER}
 
-mkdir -p "${GRADLE_USER_HOME}"
-
 SSH_DIR=/home/${BUILD_USER}/.ssh
 mkdir -p ${SSH_DIR}
 chown ${BUILD_USER} ${SSH_DIR}
 
-echo "Claiming ownership of mounted gradle dirs:"
-find "${GRADLE_USER_HOME}" -maxdepth 3 -type d -not -user ${BUILD_USER} -print -execdir chown ${BUILD_USER} {} \+
+if [[ -z "${GRADLE_USER_HOME}" ]]; then
+    echo "GRADLE_USER_HOME is not set; mounted caches and/or wrapper could work incorrectly"
+else
+    echo "GRADLE_USER_HOME is set; Claiming ownership of mounted gradle dirs:"
+    mkdir -p "${GRADLE_USER_HOME}"
+    find "${GRADLE_USER_HOME}" -maxdepth 3 -type d -not -user ${BUILD_USER} -print -execdir chown ${BUILD_USER} {} \+
+fi
 
 # shellcheck disable=SC2145
 echo "Running command: $@"
