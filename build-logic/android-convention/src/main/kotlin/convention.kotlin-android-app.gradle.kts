c0763fcac7a54bf8332b305ca652e260b174c540
Dmitriy Voronin <dsvoronin@gmail.com>
2021-02-18 16:37:51 +0300

    MBS-10559 add missing library variant configs (#790)
    add android.buildFeatures config
    add compileAll task

--- build-logic/android-convention/src/main/kotlin/convention.kotlin-android-app.gradle.kts
+++ build-logic/android-convention/src/main/kotlin/convention.kotlin-android-app.gradle.kts
@@ -4,3 +4,26 @@ plugins {
     id("convention.kotlin-base")
     id("convention.android-base")
 }
+
+android {
+
+    testBuildType = "debug"
+
+    /**
+     * Disable all buildTypes except testing
+     * to avoid confusing errors in IDE if wrong build variant is selected
+     */
+    variantFilter {
+        if (name != testBuildType) {
+            ignore = true
+            logger.debug("Build variant $name is omitted for module: $path")
+        }
+    }
+
+    buildTypes {
+        getByName(testBuildType) {
+            // libraries only built in release variant, see convention.kotlin-android-library
+            matchingFallbacks += listOf("release")
+        }
+    }
+}
