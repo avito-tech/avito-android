e8789477e14b93716327becbc284fc611619d9fd
Dmitriy Voronin <dvoronin@avito.ru>
2021-02-20 00:55:54 +0300

    MBS-10666 fix problem with project version resolving
    avito.project.version not used anymore

--- build-logic/publication/src/main/kotlin/convention.publish-base.gradle.kts
+++ build-logic/publication/src/main/kotlin/convention.publish-base.gradle.kts
@@ -4,10 +4,9 @@ plugins {
 
 group = "com.avito.android"
 
-// avito.project.version override from CI property, looks like it could be simplified to single one
 @Suppress("UnstableApiUsage")
-version = providers.systemProperty("avito.project.version").forUseAtConfigurationTime()
-    .orElse(providers.gradleProperty("projectVersion").forUseAtConfigurationTime())
+version = providers.gradleProperty("projectVersion")
+    .forUseAtConfigurationTime()
     .get()
 
 publishing.publications.withType<MavenPublication> {
