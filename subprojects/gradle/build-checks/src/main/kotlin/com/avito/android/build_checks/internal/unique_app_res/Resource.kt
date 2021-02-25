d7629e03f2f9ad778a51caba1f359fdae08b7f76
Evgeniy Krivobokov <ekrivobokov@avito.ru>
2021-02-25 15:58:13 +0300

    MBS-10577 Fix unique app resources exceptions

--- subprojects/gradle/build-checks/src/main/kotlin/com/avito/android/build_checks/internal/unique_app_res/Resource.kt
+++ subprojects/gradle/build-checks/src/main/kotlin/com/avito/android/build_checks/internal/unique_app_res/Resource.kt
@@ -2,7 +2,7 @@ package com.avito.android.build_checks.internal.unique_app_res
 
 import com.android.resources.ResourceType
 
-internal data class Resource(
+public data class Resource(
     val type: ResourceType,
     val name: String
 ) : java.io.Serializable
