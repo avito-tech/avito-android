1a8aa9afccc769ab762802ccad40841037fe34bc
Sergey Boishtyan <ssboishtyan@avito.ru>
2021-03-09 22:24:03 +0300

    Fix sonatype publication

--- ci/publish.sh
+++ ci/publish.sh
@@ -1,7 +1,16 @@
 #!/usr/bin/env bash
 
 # This is an entrypoint for CI build step, don't change it's relative path(name)
+# force Envs be set up
+set -u
 
 source $(dirname $0)/_main.sh
+local readonly profileId="-Pavito.ossrh.stagingProfileId=${$OSSRH_STAGING_ID}"
+local readonly user="-Pavito.ossrh.user=${OSSRH_USER}"
+local readonly password="-Pavito.ossrh.password=${OSSRH_PASSWORD}"
 
-runInBuilder "./gradlew -p subprojects publishRelease ${GRADLE_ARGS} --no-parallel --stacktrace"
+local readonly CREDENTIALS = "${profileId} ${user} ${password}"
+
+local readonly PGP = "-Pavito.pgp.keyid=${$PGP_KEY_ID} -Pavito.pgp.key=${PGP_KEY} -Pavito.pgp.password=${PGP_PASSWORD}"
+
+runInBuilder "./gradlew -p subprojects publishRelease ${GRADLE_ARGS} ${CREDENTIALS} ${PGP} --no-parallel --stacktrace"
