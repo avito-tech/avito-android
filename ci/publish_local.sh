ff895f830aae59f2cd810176ae59c645de5112da
Dmitriy Voronin <dsvoronin@gmail.com>
2021-03-15 14:31:24 +0300

    MBS-10805 remove artifactory publishing (#839)
    publish only to mavenCentral or mavenLocal(for testing)
    
    use integration teamcity builds for ci testing

--- ci/publish_local.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-# This is an entrypoint for CI build step, don't change it's relative path(name)
-
-source $(dirname $0)/_main.sh
-
-runInBuilder "./gradlew -p subprojects publishToArtifactory ${GRADLE_ARGS} -Dorg.gradle.internal.publish.checksums.insecure=true"
