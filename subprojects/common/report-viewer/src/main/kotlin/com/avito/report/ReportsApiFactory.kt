8e7df5ca37e0c73925e0c0f8130a8a1b1aa7ce13
Dmitriy Voronin <dvoronin@avito.ru>
2021-03-15 19:01:29 +0300

    MBS-10798 describe jsonrpc method in RetryInterceptor

--- subprojects/common/report-viewer/src/main/kotlin/com/avito/report/ReportsApiFactory.kt
+++ subprojects/common/report-viewer/src/main/kotlin/com/avito/report/ReportsApiFactory.kt
@@ -6,9 +6,14 @@ import com.avito.logger.LoggerFactory
 import com.avito.logger.create
 import com.avito.report.internal.JsonRpcRequestProvider
 import com.avito.report.internal.getHttpClient
+import com.avito.report.internal.model.RfcRpcRequest
 import com.avito.report.model.EntryTypeAdapterFactory
+import com.github.salomonbrys.kotson.fromJson
 import com.google.gson.Gson
 import com.google.gson.GsonBuilder
+import com.google.gson.JsonParseException
+import okhttp3.Request
+import okio.Buffer
 
 object ReportsApiFactory {
 
@@ -32,7 +37,8 @@ object ReportsApiFactory {
         logger: Logger = loggerFactory.create<ReportsApi>(),
         retryInterceptor: RetryInterceptor? = RetryInterceptor(
             logger = logger,
-            allowedMethods = listOf("POST")
+            allowedMethods = listOf("POST"),
+            describeRequest = { it.describeJsonRpc() }
         )
     ): ReportsApi {
         return ReportsApiImpl(
@@ -49,5 +55,25 @@ object ReportsApiFactory {
                 gson = gson
             )
         )
+    }
+}
+
+private fun Request.describeJsonRpc(): String {
+    return "${url.redact()} method: ${jsonRpcMethod()}"
+}
+
+private fun Request.jsonRpcMethod(): String {
+    val bodyCopy = newBuilder().build()
+    val buffer = Buffer()
+    bodyCopy.body?.writeTo(buffer)
+
+    // this is suboptimal, should not be read fully
+    val json = buffer.readUtf8()
+
+    return try {
+        val jsonRpcRequest = ReportsApiFactory.gson.fromJson<RfcRpcRequest>(json)
+        jsonRpcRequest.method
+    } catch (e: JsonParseException) {
+        "unknown"
     }
 }
