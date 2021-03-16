6e6668c6632345e5f286a3512846fb1df152cdd4
Dmitriy Voronin <dsvoronin@gmail.com>
2021-03-16 15:38:13 +0300

    MBS-10798 add missing plugins for build-logic and libraries (#845)

--- build-logic/settings.gradle.kts
+++ build-logic/settings.gradle.kts
@@ -5,6 +5,31 @@ include("android-convention")
 include("testing-convention")
 include("publication")
 
+pluginManagement {
+    val artifactoryUrl: String? by settings
+
+    fun MavenArtifactRepository.artifactoryUrl(repositoryName: String) {
+        setUrl("$artifactoryUrl/$repositoryName")
+        isAllowInsecureProtocol = true
+    }
+
+    fun MavenArtifactRepository.setUrlOrProxy(repositoryName: String, originalRepo: String) {
+        if (artifactoryUrl.isNullOrBlank()) {
+            name = repositoryName
+            setUrl(originalRepo)
+        } else {
+            name = "Proxy for $repositoryName: $originalRepo"
+            artifactoryUrl(repositoryName)
+        }
+    }
+
+    repositories {
+        maven {
+            setUrlOrProxy("gradle-plugins", "https://plugins.gradle.org/m2/")
+        }
+    }
+}
+
 val artifactoryUrl: String? by settings
 
 fun MavenArtifactRepository.artifactoryUrl(repositoryName: String) {
