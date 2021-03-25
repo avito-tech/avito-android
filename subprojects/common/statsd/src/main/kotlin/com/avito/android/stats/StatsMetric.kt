76e9bf0aae236027028290aebb5007c212ea5db1
Dmitriy Voronin <dsvoronin@gmail.com>
2021-03-25 20:34:12 +0300

    MBS-10704 metrics as data classes

--- subprojects/common/statsd/src/main/kotlin/com/avito/android/stats/StatsMetric.kt
+++ subprojects/common/statsd/src/main/kotlin/com/avito/android/stats/StatsMetric.kt
@@ -6,92 +6,26 @@ sealed class StatsMetric {
     abstract val type: String
 }
 
-class TimeMetric(
+data class TimeMetric(
     override val name: SeriesName,
-    timeInMs: Long
+    val timeInMs: Long
 ) : StatsMetric() {
     override val value: Long = timeInMs
     override val type = "time"
-
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (javaClass != other?.javaClass) return false
-
-        other as TimeMetric
-
-        if (name != other.name) return false
-        if (value != other.value) return false
-
-        return true
-    }
-
-    override fun hashCode(): Int {
-        var result = name.hashCode()
-        result = 31 * result + value.hashCode()
-        return result
-    }
-
-    override fun toString(): String {
-        return "TimeMetric(name=$name, value=$value)"
-    }
 }
 
-class CountMetric(
+data class CountMetric(
     override val name: SeriesName,
-    delta: Long = 1
+    val delta: Long = 1
 ) : StatsMetric() {
     override val value: Long = delta
     override val type = "count"
-
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (javaClass != other?.javaClass) return false
-
-        other as CountMetric
-
-        if (name != other.name) return false
-        if (value != other.value) return false
-
-        return true
-    }
-
-    override fun hashCode(): Int {
-        var result = name.hashCode()
-        result = 31 * result + value.hashCode()
-        return result
-    }
-
-    override fun toString(): String {
-        return "CountMetric(name=$name, value=$value)"
-    }
 }
 
-class GaugeMetric(
+data class GaugeMetric(
     override val name: SeriesName,
-    value: Number
+    val gauge: Number
 ) : StatsMetric() {
-    override val value: Long = value.toLong()
+    override val value: Long = gauge.toLong()
     override val type = "gauge"
-
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (javaClass != other?.javaClass) return false
-
-        other as GaugeMetric
-
-        if (name != other.name) return false
-        if (value != other.value) return false
-
-        return true
-    }
-
-    override fun hashCode(): Int {
-        var result = name.hashCode()
-        result = 31 * result + value.hashCode()
-        return result
-    }
-
-    override fun toString(): String {
-        return "GaugeMetric(name=$name, value=$value)"
-    }
 }
