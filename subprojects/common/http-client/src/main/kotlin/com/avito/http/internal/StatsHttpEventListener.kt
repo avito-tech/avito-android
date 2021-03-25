cfbaab07ed922262fdb01477364308647aa06889
Dmitriy Voronin <dsvoronin@gmail.com>
2021-03-25 20:16:45 +0300

    MBS-10704 send unknown metrics; add request info to warning message

--- subprojects/common/http-client/src/main/kotlin/com/avito/http/internal/StatsHttpEventListener.kt
+++ subprojects/common/http-client/src/main/kotlin/com/avito/http/internal/StatsHttpEventListener.kt
@@ -29,37 +29,40 @@ internal class StatsHttpEventListener(
 
     override fun responseHeadersEnd(call: Call, response: Response) {
         val prefix = serviceMetric(call.request())
-        if (prefix != null) {
-            val latencyMs = response.receivedResponseAtMillis - response.sentRequestAtMillis
-            statsDSender.send(TimeMetric(prefix.append("${response.code}"), latencyMs))
-        }
+
+        val latencyMs = response.receivedResponseAtMillis - response.sentRequestAtMillis
+        statsDSender.send(TimeMetric(prefix.append("${response.code}"), latencyMs))
     }
 
     override fun callFailed(call: Call, ioe: IOException) {
         val prefix = serviceMetric(call.request())
 
-        if (prefix != null) {
-            val latencyMs = timeProvider.nowInMillis() - callStarted
+        val latencyMs = timeProvider.nowInMillis() - callStarted
 
-            if (ioe is SocketTimeoutException) {
-                statsDSender.send(TimeMetric(prefix.append("timeout"), latencyMs))
-            } else {
-                statsDSender.send(TimeMetric(prefix.append("unknown"), latencyMs))
-            }
+        if (ioe is SocketTimeoutException) {
+            statsDSender.send(TimeMetric(prefix.append("timeout"), latencyMs))
+        } else {
+            statsDSender.send(TimeMetric(prefix.append("unknown"), latencyMs))
         }
     }
 
-    private fun serviceMetric(request: Request): SeriesName? {
+    private fun serviceMetric(request: Request): SeriesName {
         val metadata = request.tag(RequestMetadata::class.java)
 
         return if (metadata == null) {
+            val urlWithoutParams = request.url.newBuilder().query(null).build()
+
             logger.warn(
-                msg = "RequestMetadata not available\n" +
+                msg = "RequestMetadata not available for: $urlWithoutParams\n" +
                     "You should add okhttp.Request.tag() with additional request information for metrics\n" +
                     "or use Retrofit @Tag annotation",
                 error = null
             )
-            null
+            SeriesName.create(
+                "service",
+                "unknown-service",
+                "unknown-method"
+            )
         } else {
             SeriesName.create(
                 "service",
