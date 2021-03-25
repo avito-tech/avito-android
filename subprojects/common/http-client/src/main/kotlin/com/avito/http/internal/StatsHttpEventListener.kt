f063bb9063181411cdd127dfe389773e2e395af2
Dmitriy Voronin <dsvoronin@gmail.com>
2021-03-25 19:10:31 +0300

    MBS-10704 add docs

--- subprojects/common/http-client/src/main/kotlin/com/avito/http/internal/StatsHttpEventListener.kt
+++ subprojects/common/http-client/src/main/kotlin/com/avito/http/internal/StatsHttpEventListener.kt
@@ -3,6 +3,8 @@ package com.avito.http.internal
 import com.avito.android.stats.SeriesName
 import com.avito.android.stats.StatsDSender
 import com.avito.android.stats.TimeMetric
+import com.avito.logger.LoggerFactory
+import com.avito.logger.create
 import com.avito.time.TimeProvider
 import okhttp3.Call
 import okhttp3.EventListener
@@ -13,8 +15,11 @@ import java.net.SocketTimeoutException
 
 internal class StatsHttpEventListener(
     private val statsDSender: StatsDSender,
-    private val timeProvider: TimeProvider
+    private val timeProvider: TimeProvider,
+    loggerFactory: LoggerFactory
 ) : EventListener() {
+
+    private val logger = loggerFactory.create<StatsHttpEventListener>()
 
     var callStarted = 0L
 
@@ -23,33 +28,40 @@ internal class StatsHttpEventListener(
     }
 
     override fun responseHeadersEnd(call: Call, response: Response) {
-        val latencyMs = response.receivedResponseAtMillis - response.sentRequestAtMillis
-        statsDSender.send(TimeMetric(serviceMetric(call.request()).append("${response.code}"), latencyMs))
+        val prefix = serviceMetric(call.request())
+        if (prefix != null) {
+            val latencyMs = response.receivedResponseAtMillis - response.sentRequestAtMillis
+            statsDSender.send(TimeMetric(prefix.append("${response.code}"), latencyMs))
+        }
     }
 
     override fun callFailed(call: Call, ioe: IOException) {
         val prefix = serviceMetric(call.request())
 
-        val latencyMs = timeProvider.nowInMillis() - callStarted
+        if (prefix != null) {
+            val latencyMs = timeProvider.nowInMillis() - callStarted
 
-        if (ioe is SocketTimeoutException) {
-            statsDSender.send(TimeMetric(prefix.append("timeout"), latencyMs))
-        } else {
-            statsDSender.send(TimeMetric(prefix.append("unknown"), latencyMs))
+            if (ioe is SocketTimeoutException) {
+                statsDSender.send(TimeMetric(prefix.append("timeout"), latencyMs))
+            } else {
+                statsDSender.send(TimeMetric(prefix.append("unknown"), latencyMs))
+            }
         }
     }
 
-    private fun serviceMetric(request: Request): SeriesName {
+    private fun serviceMetric(request: Request): SeriesName? {
         val metadata = request.tag(RequestMetadata::class.java)
 
-        if (metadata == null) {
-            throw IllegalStateException(
-                "RequestMetadata not available\n" +
+        return if (metadata == null) {
+            logger.warn(
+                msg = "RequestMetadata not available\n" +
                     "You should add okhttp.Request.tag() with additional request information for metrics\n" +
-                    "or use Retrofit @Tag annotation"
+                    "or use Retrofit @Tag annotation",
+                error = null
             )
+            null
         } else {
-            return SeriesName.create(
+            SeriesName.create(
                 "service",
                 metadata.serviceName,
                 metadata.methodName
