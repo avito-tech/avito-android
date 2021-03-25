13edec655dbff8e6ec9b786c982ba5b90dc52104
Dmitriy Voronin <dsvoronin@gmail.com>
2021-03-25 20:30:11 +0300

    MBS-10704 add parallel requests test

--- subprojects/common/http-client/src/test/kotlin/com/avito/http/HttpClientProviderMetricsTest.kt
+++ subprojects/common/http-client/src/test/kotlin/com/avito/http/HttpClientProviderMetricsTest.kt
@@ -9,14 +9,19 @@ import com.avito.logger.StubLoggerFactory
 import com.avito.time.StubTimeProvider
 import com.google.common.truth.Correspondence
 import com.google.common.truth.Truth.assertThat
+import okhttp3.Call
+import okhttp3.Callback
 import okhttp3.OkHttpClient
 import okhttp3.Request
+import okhttp3.Response
 import okhttp3.mockwebserver.MockResponse
 import okhttp3.mockwebserver.MockWebServer
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
+import java.io.IOException
 import java.net.SocketTimeoutException
+import java.util.concurrent.CountDownLatch
 import java.util.concurrent.TimeUnit
 
 internal class HttpClientProviderMetricsTest {
@@ -41,7 +46,7 @@ internal class HttpClientProviderMetricsTest {
     private val doesNotMatter = 111L
 
     @Test
-    fun `timeout metric`() {
+    fun `timeout metric send - request timed out`() {
         val provider = createClientProvider()
 
         val httpClient = provider.provide()
@@ -51,7 +56,7 @@ internal class HttpClientProviderMetricsTest {
         mockWebServer.enqueue(MockResponse().setHeadersDelay(10, TimeUnit.MILLISECONDS))
 
         assertThrows<SocketTimeoutException> {
-            httpClient.call()
+            httpClient.blockingCall()
         }
 
         assertThat(statsDSender.getSentMetrics()).comparingElementsUsing(metricNamesCorrespondence).containsExactly(
@@ -60,14 +65,35 @@ internal class HttpClientProviderMetricsTest {
     }
 
     @Test
-    fun `response 200 metric`() {
+    fun `all metrics send - multiple parallel requests`() {
+        val provider = createClientProvider()
+
+        val httpClient = provider.provide().build()
+
+        val count = 3
+
+        repeat(count) {
+            mockWebServer.enqueue(MockResponse().setResponseCode(200))
+        }
+
+        httpClient.blockingMultipleParallelCalls(count)
+
+        assertThat(statsDSender.getSentMetrics()).comparingElementsUsing(metricNamesCorrespondence).containsExactly(
+            TimeMetric(SeriesName.create("service", "some-service", "some-method", "200"), doesNotMatter),
+            TimeMetric(SeriesName.create("service", "some-service", "some-method", "200"), doesNotMatter),
+            TimeMetric(SeriesName.create("service", "some-service", "some-method", "200"), doesNotMatter)
+        )
+    }
+
+    @Test
+    fun `response 200 metric send - success request`() {
         val provider = createClientProvider()
 
         val httpClient = provider.provide().build()
 
         mockWebServer.enqueue(MockResponse().setResponseCode(200))
 
-        httpClient.call()
+        httpClient.blockingCall()
 
         assertThat(statsDSender.getSentMetrics()).comparingElementsUsing(metricNamesCorrespondence).contains(
             TimeMetric(SeriesName.create("service", "some-service", "some-method", "200"), doesNotMatter)
@@ -75,14 +101,14 @@ internal class HttpClientProviderMetricsTest {
     }
 
     @Test
-    fun `response 502 metric`() {
+    fun `response 502 metric - failed request`() {
         val provider = createClientProvider()
 
         val httpClient = provider.provide().build()
 
         mockWebServer.enqueue(MockResponse().setResponseCode(502))
 
-        httpClient.call()
+        httpClient.blockingCall()
 
         assertThat(statsDSender.getSentMetrics()).comparingElementsUsing(metricNamesCorrespondence).contains(
             TimeMetric(SeriesName.create("service", "some-service", "some-method", "502"), doesNotMatter)
@@ -90,7 +116,7 @@ internal class HttpClientProviderMetricsTest {
     }
 
     @Test
-    fun `retries counts`() {
+    fun `all retries metrics send - request retries multiple times`() {
         val provider = createClientProvider()
 
         val httpClient = provider.provide()
@@ -101,7 +127,7 @@ internal class HttpClientProviderMetricsTest {
         mockWebServer.enqueue(MockResponse().setResponseCode(500))
         mockWebServer.enqueue(MockResponse().setResponseCode(200))
 
-        httpClient.call()
+        httpClient.blockingCall()
 
         assertThat(statsDSender.getSentMetrics()).comparingElementsUsing(metricNamesCorrespondence).containsAtLeast(
             TimeMetric(SeriesName.create("service", "some-service", "some-method", "502"), doesNotMatter),
@@ -115,13 +141,38 @@ internal class HttpClientProviderMetricsTest {
         mockWebServer.shutdown()
     }
 
-    private fun OkHttpClient.call() {
-        newCall(
+    private fun OkHttpClient.createCall(): Call {
+        return newCall(
             Request.Builder()
                 .url(mockWebServer.url("/"))
-                .tag(RequestMetadata::class.java, RequestMetadata("some-service", "some-method"))
+                .tag(
+                    RequestMetadata::class.java,
+                    RequestMetadata("some-service", "some-method")
+                )
                 .build()
-        ).execute()
+        )
+    }
+
+    private fun OkHttpClient.blockingCall() {
+        createCall().execute()
+    }
+
+    private fun OkHttpClient.blockingMultipleParallelCalls(count: Int) {
+        val latch = CountDownLatch(count)
+        repeat(count) {
+            createCall().enqueue(
+                object : Callback {
+                    override fun onFailure(call: Call, e: IOException) {
+                        latch.countDown()
+                    }
+
+                    override fun onResponse(call: Call, response: Response) {
+                        latch.countDown()
+                    }
+                }
+            )
+        }
+        latch.await(5, TimeUnit.SECONDS)
     }
 
     private fun createClientProvider(): HttpClientProvider {
