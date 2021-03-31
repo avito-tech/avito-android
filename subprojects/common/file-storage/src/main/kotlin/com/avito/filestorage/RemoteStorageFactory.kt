fcf642ba2570741c684cc54a370be4c3bf6c5af8
Sergey Boishtyan <ssboishtyan@avito.ru>
2021-03-31 13:44:41 +0300

    Reduce file storage timeouts

--- subprojects/common/file-storage/src/main/kotlin/com/avito/filestorage/RemoteStorageFactory.kt
+++ subprojects/common/file-storage/src/main/kotlin/com/avito/filestorage/RemoteStorageFactory.kt
@@ -8,21 +8,24 @@ import java.util.concurrent.TimeUnit
 
 object RemoteStorageFactory {
 
-    private const val TIMEOUT_SEC = 120L
+    private const val TIMEOUT_SEC = 30L
+    private const val CONNECTION_TIMEOUT_SEC = 10L
 
     fun create(
         endpoint: String,
         httpClientProvider: HttpClientProvider,
         loggerFactory: LoggerFactory,
         timeProvider: TimeProvider,
-    ): RemoteStorage = HttpRemoteStorage(
-        endpoint = requireNotNull(endpoint.toHttpUrlOrNull()) { "Can't parse endpoint: $endpoint" },
-        httpClient = httpClientProvider.provide()
-            .connectTimeout(TIMEOUT_SEC, TimeUnit.SECONDS)
-            .writeTimeout(TIMEOUT_SEC, TimeUnit.SECONDS)
-            .readTimeout(TIMEOUT_SEC, TimeUnit.SECONDS)
-            .build(),
-        loggerFactory = loggerFactory,
-        timeProvider = timeProvider
-    )
+    ): RemoteStorage {
+        return HttpRemoteStorage(
+            endpoint = requireNotNull(endpoint.toHttpUrlOrNull()) { "Can't parse endpoint: $endpoint" },
+            httpClient = httpClientProvider.provide()
+                .connectTimeout(CONNECTION_TIMEOUT_SEC, TimeUnit.SECONDS)
+                .writeTimeout(TIMEOUT_SEC, TimeUnit.SECONDS)
+                .readTimeout(TIMEOUT_SEC, TimeUnit.SECONDS)
+                .build(),
+            loggerFactory = loggerFactory,
+            timeProvider = timeProvider
+        )
+    }
 }
