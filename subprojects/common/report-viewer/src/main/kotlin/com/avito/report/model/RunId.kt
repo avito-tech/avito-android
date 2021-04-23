f65b2fcfe758604509434b1314fa076a5ebe0f0e
Ruslan Mingaliev <mingaliev.rr@gmail.com>
2021-04-23 15:02:52 +0300

    Remove unused RunId#fromString method
    Signed-off-by: Ruslan Mingaliev <mingaliev.rr@gmail.com>

--- subprojects/common/report-viewer/src/main/kotlin/com/avito/report/model/RunId.kt
+++ subprojects/common/report-viewer/src/main/kotlin/com/avito/report/model/RunId.kt
@@ -1,7 +1,5 @@
 package com.avito.report.model
 
-import com.avito.android.Result
-
 /**
  * Universal identifier for test suits
  * @param prefix optional prefix
@@ -14,12 +12,6 @@ data class RunId(
     val buildTypeId: String
 ) {
 
-    init {
-        require(!prefix.contains(DELIMITER)) { "prefix could not contain '$DELIMITER' character" }
-        require(!commitHash.contains(DELIMITER)) { "commitHash could not contain '$DELIMITER' character" }
-        require(!buildTypeId.contains(DELIMITER)) { "buildTypeId could not contain '$DELIMITER' character" }
-    }
-
     override fun toString(): String = when {
         prefix.isBlank() -> "$commitHash$DELIMITER$buildTypeId"
         else -> "$prefix$DELIMITER$commitHash$DELIMITER$buildTypeId"
@@ -27,27 +19,6 @@ data class RunId(
 
     companion object {
 
-        const val DELIMITER = ':'
-
-        fun fromString(runId: String): Result<RunId> {
-            val split = runId.split(DELIMITER)
-
-            return when (split.size) {
-                3 -> Result.Success(
-                    RunId(
-                        prefix = split.component1(),
-                        commitHash = split.component2(),
-                        buildTypeId = split.component3()
-                    )
-                )
-                2 -> Result.Success(
-                    RunId(
-                        commitHash = split.component1(),
-                        buildTypeId = split.component2()
-                    )
-                )
-                else -> Result.Failure(IllegalArgumentException("Invalid runId: $runId"))
-            }
-        }
+        internal const val DELIMITER = '.'
     }
 }
--- subprojects/common/report-viewer/src/test/kotlin/com/avito/report/model/RunIdTest.kt
+++ subprojects/common/report-viewer/src/test/kotlin/com/avito/report/model/RunIdTest.kt
@@ -1,8 +1,6 @@
 package com.avito.report.model
 
-import com.avito.android.Result
 import com.google.common.truth.Truth.assertThat
-import org.junit.Assert.assertThrows
 import org.junit.Test
 
 internal class RunIdTest {
@@ -26,40 +24,6 @@ internal class RunIdTest {
         val actual = RunId(prefix = "   ", commitHash = COMMIT_HASH, buildTypeId = BUILD_TYPE_ID).toString()
         val expected = "$COMMIT_HASH${RunId.DELIMITER}$BUILD_TYPE_ID"
         assertThat(actual).isEqualTo(expected)
-    }
-
-    @Test
-    fun `deserialize - without prefix`() {
-        val actual = RunId.fromString("$COMMIT_HASH${RunId.DELIMITER}$BUILD_TYPE_ID")
-        val expected = Result.Success(RunId(commitHash = COMMIT_HASH, buildTypeId = BUILD_TYPE_ID))
-        assertThat(actual).isEqualTo(expected)
-    }
-
-    @Test
-    fun `deserialize - with prefix`() {
-        val actual = RunId.fromString("$PREFIX${RunId.DELIMITER}$COMMIT_HASH${RunId.DELIMITER}$BUILD_TYPE_ID")
-        val expected = Result.Success(RunId(prefix = PREFIX, commitHash = COMMIT_HASH, buildTypeId = BUILD_TYPE_ID))
-        assertThat(actual).isEqualTo(expected)
-    }
-
-    @Test
-    fun `deserialize - invalid parameter`() {
-        val actual = RunId.fromString("1234567890abc")
-        assertThat(actual).isInstanceOf(Result.Failure::class.java)
-        assertThat((actual as Result.Failure).throwable).isInstanceOf(IllegalArgumentException::class.java)
-    }
-
-    @Test
-    fun `parameters could not contain delimiter`() {
-        assertThrows(IllegalArgumentException::class.java) {
-            RunId(RunId.DELIMITER.toString(), COMMIT_HASH, BUILD_TYPE_ID)
-        }
-        assertThrows(IllegalArgumentException::class.java) {
-            RunId(PREFIX, RunId.DELIMITER.toString(), BUILD_TYPE_ID)
-        }
-        assertThrows(IllegalArgumentException::class.java) {
-            RunId(PREFIX, COMMIT_HASH, RunId.DELIMITER.toString())
-        }
     }
 
     private companion object {
