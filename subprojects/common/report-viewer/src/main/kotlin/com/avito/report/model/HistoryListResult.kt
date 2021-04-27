4a01b4577278438f66bbe5f410787f13dd3f4e45
Dmitriy Voronin <dsvoronin@gmail.com>
2021-04-28 00:20:24 +0300

    remove unused code

--- subprojects/common/report-viewer/src/main/kotlin/com/avito/report/model/HistoryListResult.kt
+++
@@ -1,8 +0,0 @@
-package com.avito.report.model
-
-import com.google.gson.annotations.SerializedName
-
-data class HistoryListResult(
-    @SerializedName("test_name") val testName: String,
-    val records: List<HistoryTest>?
-)
--- subprojects/common/report-viewer/src/main/kotlin/com/avito/report/model/PerformanceTest.kt
+++
@@ -1,8 +0,0 @@
-package com.avito.report.model
-
-// todo extends CompletedTest?
-class PerformanceTest(
-    val testName: String,
-    val id: String,
-    val series: Map<String, List<Double>>
-)
