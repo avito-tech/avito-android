56c2df10085f96826f53b2663e8144fa6777f912
Evgeniy Krivobokov <ekrivobokov@avito.ru>
2021-04-29 19:29:34 +0300

    MBS-6015 Log unknown build cache errors

--- subprojects/gradle/build-metrics/src/gradleTest/kotlin/com/avito/android/plugin/build_metrics/cache/HttpBuildCacheMetricsTest.kt
+++ subprojects/gradle/build-metrics/src/gradleTest/kotlin/com/avito/android/plugin/build_metrics/cache/HttpBuildCacheMetricsTest.kt
@@ -4,9 +4,11 @@ import com.avito.android.plugin.build_metrics.assertHasMetric
 import com.avito.android.plugin.build_metrics.assertNoMetric
 import com.avito.android.stats.CountMetric
 import com.avito.android.stats.StatsMetric
+import com.avito.test.gradle.TestResult
 import com.google.common.truth.Truth.assertThat
 import org.gradle.testkit.runner.TaskOutcome
-import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.DynamicTest
+import org.junit.jupiter.api.TestFactory
 import java.io.File
 
 internal class HttpBuildCacheMetricsTest : HttpBuildCacheTestFixture() {
@@ -41,46 +43,86 @@ internal class HttpBuildCacheMetricsTest : HttpBuildCacheTestFixture() {
         )
     }
 
-    @Test
-    fun `no errors - miss and successful store`() {
-        givenHttpBuildCache(loadHttpStatus = 404, storeHttpStatus = 200)
+    private class TestCase(
+        val name: String,
+        val loadStatus: Int,
+        val storeStatus: Int,
+        val assertion: (result: TestResult) -> Unit
+    )
 
-        val result = build(":cacheMissTask")
+    private val cases = listOf(
+        TestCase(
+            name = "no errors - miss and successful store",
+            loadStatus = 404,
+            storeStatus = 200,
+            assertion = { result ->
+                result.assertNoMetric<StatsMetric>(".build.cache.errors.")
+            }
+        ),
+        TestCase(
+            name = "load error - 500 response",
+            loadStatus = 500,
+            storeStatus = 200,
+            assertion = { result ->
+                result.assertHasMetric<CountMetric>(".build.cache.errors.load.500").also {
+                    assertThat(it.delta).isEqualTo(1)
+                }
+            }
+        ),
+        TestCase(
+            name = "store error - 500 response",
+            loadStatus = 404,
+            storeStatus = 500,
+            assertion = { result ->
+                result.assertHasMetric<CountMetric>(".build.cache.errors.store.500").also {
+                    assertThat(it.delta).isEqualTo(1)
+                }
+            }
+        ),
+        TestCase(
+            name = "store error - unknown error",
+            loadStatus = 404,
+            storeStatus = invalidHttpStatus,
+            assertion = { result ->
+                result.assertHasMetric<CountMetric>(".build.cache.errors.store.unknown").also {
+                    assertThat(it.delta).isEqualTo(1)
+                }
+            }
+        ),
+        TestCase(
+            name = "load error - unknown error",
+            loadStatus = invalidHttpStatus,
+            storeStatus = 200,
+            assertion = { result ->
+                result.assertHasMetric<CountMetric>(".build.cache.errors.load.unknown").also {
+                    assertThat(it.delta).isEqualTo(1)
+                }
+            }
+        ),
+    )
 
-        result.assertThat()
-            .buildSuccessful()
-            .taskWithOutcome(":cacheMissTask", TaskOutcome.SUCCESS)
+    @TestFactory
+    fun `remote cache errors`(): List<DynamicTest> {
+        return cases.map { case ->
+            DynamicTest.dynamicTest(case.name) {
+                setup()
 
-        result.assertNoMetric<StatsMetric>(".build.cache.errors.")
-    }
+                try {
+                    givenHttpBuildCache(loadHttpStatus = case.loadStatus, storeHttpStatus = case.storeStatus)
 
-    @Test
-    fun `load error - 500 response`() {
-        givenHttpBuildCache(loadHttpStatus = 500, storeHttpStatus = 200)
+                    val result = build(":cacheMissTask")
 
-        val result = build(":cacheMissTask")
+                    result.assertThat()
+                        .buildSuccessful()
+                        .taskWithOutcome(":cacheMissTask", TaskOutcome.SUCCESS)
 
-        result.assertThat()
-            .buildSuccessful()
-            .taskWithOutcome(":cacheMissTask", TaskOutcome.SUCCESS)
-
-        result.assertHasMetric<CountMetric>(".build.cache.errors.load.500").also {
-            assertThat(it.delta).isEqualTo(1)
-        }
-    }
-
-    @Test
-    fun `store error - 500 response`() {
-        givenHttpBuildCache(loadHttpStatus = 404, storeHttpStatus = 500)
-
-        val result = build(":cacheMissTask")
-
-        result.assertThat()
-            .buildSuccessful()
-            .taskWithOutcome(":cacheMissTask", TaskOutcome.SUCCESS)
-
-        result.assertHasMetric<CountMetric>(".build.cache.errors.store.500").also {
-            assertThat(it.delta).isEqualTo(1)
+                    case.assertion(result)
+                } finally {
+                    cleanup()
+                }
+            }
         }
     }
 }
+
+private const val invalidHttpStatus = -1
