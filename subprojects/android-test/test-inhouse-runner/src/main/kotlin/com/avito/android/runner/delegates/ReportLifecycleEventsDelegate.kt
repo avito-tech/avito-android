8ce5d8b2c6cfbab75fed94cc6b75d6c4f5cd8899
Sergey Boishtyan <ssboishtyan@avito.ru>
2021-05-14 15:11:48 +0300

    MBS-10887 Add lifecycle events to reports

--- subprojects/android-test/test-inhouse-runner/src/main/kotlin/com/avito/android/runner/delegates/ReportLifecycleEventsDelegate.kt
+++ subprojects/android-test/test-inhouse-runner/src/main/kotlin/com/avito/android/runner/delegates/ReportLifecycleEventsDelegate.kt
@@ -3,17 +3,19 @@ package com.avito.android.runner.delegates
 import android.os.Bundle
 import androidx.test.runner.lifecycle.ActivityLifecycleMonitorRegistry
 import com.avito.android.runner.InstrumentationTestRunnerDelegate
+import com.avito.android.test.report.Report
 import com.avito.android.test.report.lifecycle.ReportActivityLifecycleListener
 import com.avito.logger.LoggerFactory
 
 class ReportLifecycleEventsDelegate(
-    factory: LoggerFactory
+    factory: LoggerFactory,
+    report: Report
 ) : InstrumentationTestRunnerDelegate() {
     /**
      * ActivityLifecycleMonitorRegistry wraps callbacks by WeakReference.
      * So we holding a reference to avoid the garbage collection
      */
-    private val listener = ReportActivityLifecycleListener(factory)
+    private val listener = ReportActivityLifecycleListener(factory, report)
 
     override fun afterOnCreate(arguments: Bundle) {
         ActivityLifecycleMonitorRegistry
