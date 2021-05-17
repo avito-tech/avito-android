0e663eadc08060ed9457913f1e4ac4e2ffda4270
Sergey Boishtyan <ssboishtyan@avito.ru>
2021-05-17 23:01:52 +0300

    MBS-11240 Make entry loading parallel

--- subprojects/common/file-storage/src/main/kotlin/com/avito/filestorage/SettableFuture.kt
+++ subprojects/common/file-storage/src/main/kotlin/com/avito/filestorage/SettableFuture.kt
@@ -5,16 +5,27 @@ import java.util.concurrent.atomic.AtomicBoolean
 
 public interface FutureValue<T> {
 
+    /**
+     * Block current Thread until Future is ready
+     */
     public fun get(): T
+
+    /**
+     * Immediately returns. Used from coroutines
+     * @return null if Future isn't ready
+     */
+    public fun tryGet(): T?
 
     public fun <T, R> FutureValue<T>.map(transformer: (T) -> R): FutureValue<R> = object : FutureValue<R> {
         override fun get(): R = transformer(this@map.get())
+        override fun tryGet(): R? = this@map.tryGet()?.let { transformer(it) }
     }
 
     public companion object {
 
         public fun <T> create(stubValue: T): FutureValue<T> = object : FutureValue<T> {
             override fun get(): T = stubValue
+            override fun tryGet(): T? = stubValue
         }
     }
 }
@@ -22,11 +33,16 @@ public interface FutureValue<T> {
 internal class SettableFutureValue<T> : FutureValue<T> {
     private val latch = CountDownLatch(1)
     private val isSet = AtomicBoolean()
+    @Volatile
     private var slot: T? = null
 
     override fun get(): T {
         latch.await()
         return slot!!
+    }
+
+    override fun tryGet(): T? {
+        return slot
     }
 
     fun set(value: T): Boolean {
