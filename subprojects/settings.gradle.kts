5feb712ed5b084032a82947019f4a36bf4e56e6e
Eugene Krivobokov <eugene.krivobokov@gmail.com>
2021-05-19 16:39:57 +0300

    MBS-6015 Publish build scan for all CI builds (#1007)

--- subprojects/settings.gradle.kts
+++ subprojects/settings.gradle.kts
@@ -305,12 +305,16 @@ plugins {
 }
 
 val isCI = booleanProperty("ci", false)
+val buildId = stringProperty("teamcityBuildId", nullIfBlank = true)
 
 gradleEnterprise {
     buildScan {
         termsOfServiceUrl = "https://gradle.com/terms-of-service"
         termsOfServiceAgree = "yes"
-        publishOnFailureIf(isCI)
+        // Lost scans due to upload interruptions after build finishes
+        isUploadInBackground = false
+        publishAlwaysIf(isCI)
+        if (buildId != null) value("buildId", buildId)
     }
 }
 
@@ -319,5 +323,14 @@ fun booleanProperty(name: String, defaultValue: Boolean): Boolean {
         settings.extra[name]?.toString()?.toBoolean() ?: defaultValue
     } else {
         defaultValue
+    }
+}
+
+fun stringProperty(name: String, nullIfBlank: Boolean = false): String? {
+    return if (settings.extra.has(name)) {
+        val string = settings.extra[name]?.toString()
+        if (nullIfBlank && string.isNullOrBlank()) null else string
+    } else {
+        null
     }
 }
