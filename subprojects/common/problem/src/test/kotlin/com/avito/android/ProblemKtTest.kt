211cd6967e10e3f0041ce00401fc5d9bccc5abf0
Dmitriy Voronin <dsvoronin@gmail.com>
2021-05-20 11:10:33 +0300

    Update subprojects/common/problem/src/test/kotlin/com/avito/android/ProblemKtTest.kt
    Co-authored-by: Eugene Krivobokov <eugene.krivobokov@gmail.com>

--- subprojects/common/problem/src/test/kotlin/com/avito/android/ProblemKtTest.kt
+++ subprojects/common/problem/src/test/kotlin/com/avito/android/ProblemKtTest.kt
@@ -3,7 +3,7 @@ package com.avito.android
 import com.google.common.truth.Truth.assertThat
 import org.junit.jupiter.api.Test
 
-internal class ProblemKtTest {
+internal class ProblemTest {
 
     @Test
     fun `problem - as plain text - showcase`() {
