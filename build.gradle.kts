afd4593eb07bcee24ffda3e9ff6f9961c7152ff0
Evgeniy Krivobokov <ekrivobokov@avito.ru>
2021-05-20 17:49:01 +0300

    Fix lost git hooks

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,3 +14,14 @@ tasks.withType<Wrapper> {
 
     finalizedBy(subprojectsWrapper)
 }
+
+tasks.register<Exec>("installGitHooks") {
+    group = "Build Setup"
+    description = "Install local repository git hooks"
+    commandLine("git")
+    args("config", "core.hooksPath", ".git_hooks")
+    onlyIf { !project.hasProperty("ci") }
+}
+
+val initialTaskNames: List<String> = project.gradle.startParameter.taskNames
+project.gradle.startParameter.setTaskNames(initialTaskNames + listOf("installGitHooks"))
