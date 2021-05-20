c013df6d9753bed59b68aad851a115f7cccfa8da
Dmitriy Voronin <dsvoronin@gmail.com>
2021-05-20 11:01:00 +0300

    MBS-11263 composite test runner build for fast iteration testing (#1011)
    first part with only runtime dependencies

---
+++ samples/test-runner/build.gradle.kts
@@ -0,0 +1,94 @@
+import com.avito.kotlin.dsl.getOptionalStringProperty
+import com.avito.utils.gradle.kubernetesCredentials
+
+plugins {
+    id("convention.kotlin-android-app")
+    id("com.avito.android.instrumentation-tests")
+}
+
+android {
+    defaultConfig {
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+
+        // TODO: describe in docs that they are updated in IDE configuration only after sync!
+        testInstrumentationRunnerArguments(
+            mapOf(
+                "planSlug" to "AndroidTestApp"
+            )
+        )
+    }
+
+    testOptions {
+        execution = "ANDROIDX_TEST_ORCHESTRATOR"
+    }
+
+    packagingOptions {
+        pickFirst("META-INF/okhttp.kotlin_module")
+    }
+}
+
+dependencies {
+    androidTestImplementation("com.avito.android:test-inhouse-runner")
+    androidTestImplementation(libs.truth)
+
+    androidTestUtil(libs.testOrchestrator)
+}
+
+instrumentation {
+    sentryDsn = getOptionalStringProperty("avito.instrumentaion.sentry.dsn") ?: "http://stub-project@stub-host/0"
+
+    filters.register("ci") {
+        fromSource.excludeFlaky = true
+    }
+
+    experimental {
+        useInMemoryReport.set(true)
+    }
+
+    val credentials = project.kubernetesCredentials
+    if (credentials is com.avito.utils.gradle.KubernetesCredentials.Service || credentials is com.avito.utils.gradle.KubernetesCredentials.Config) {
+
+        val emulator29 = com.avito.instrumentation.reservation.request.Device.CloudEmulator(
+            name = "api29",
+            api = 29,
+            model = "Android_SDK_built_for_x86_64",
+            image = emulatorImage(29, "915c1f20be"),
+            cpuCoresRequest = "1",
+            cpuCoresLimit = "1.3",
+            memoryLimit = "4Gi"
+        )
+
+        configurationsContainer.register("ui") {
+            reportSkippedTests = true
+            filter = "ci"
+
+            targetsContainer.register("api29") {
+                deviceName = "API29"
+
+                scheduling {
+                    quota {
+                        retryCount = 1
+                        minimumSuccessCount = 1
+                    }
+
+                    testsCountBasedReservation {
+                        device = emulator29
+                        maximum = 2
+                        minimum = 1
+                        testsPerEmulator = 3
+                    }
+                }
+            }
+        }
+    }
+}
+
+val avitoRegistry = getOptionalStringProperty("avito.registry")
+
+fun emulatorImage(api: Int, label: String): String {
+    return if (avitoRegistry != null) {
+        "$avitoRegistry/android/emulator-$api:$label"
+    } else {
+        "avitotech/android-emulator-$api:$label"
+    }
+}
