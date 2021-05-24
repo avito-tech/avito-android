6dd42585d89e2389fc01d556da27f1ea4e1e4b1e
Sergey Boishtyan <ssboishtyan@avito.ru>
2021-05-24 16:39:49 +0300

    MBS-11141 Move unused in infra resolvers to the internal Avito repo

--- subprojects/android-test/ui-testing-core-app/src/androidTest/kotlin/com/avito/android/test/app/core/TestAppRunner.kt
+++ subprojects/android-test/ui-testing-core-app/src/androidTest/kotlin/com/avito/android/test/app/core/TestAppRunner.kt
@@ -5,10 +5,9 @@ import android.os.Bundle
 import com.avito.android.runner.InHouseInstrumentationTestRunner
 import com.avito.android.runner.TestRunEnvironment
 import com.avito.android.runner.annotation.resolver.AnnotationResolversBasedMetadataInjector
-import com.avito.android.runner.annotation.resolver.NetworkingResolver
 import com.avito.android.runner.annotation.resolver.TestMetadataAnnotationResolver
 import com.avito.android.runner.annotation.resolver.TestMetadataInjector
-import com.avito.android.runner.provideEnvironment
+import com.avito.android.runner.parseEnvironment
 import com.avito.android.test.report.BundleArgsProvider
 
 class TestAppRunner : InHouseInstrumentationTestRunner() {
@@ -16,12 +15,11 @@ class TestAppRunner : InHouseInstrumentationTestRunner() {
     override val metadataToBundleInjector: TestMetadataInjector = AnnotationResolversBasedMetadataInjector(
         setOf(
             TestMetadataAnnotationResolver(),
-            NetworkingResolver()
         )
     )
 
     override fun createRunnerEnvironment(arguments: Bundle): TestRunEnvironment {
-        return provideEnvironment(
+        return parseEnvironment(
             argumentsProvider = BundleArgsProvider(bundle = arguments)
         )
     }
--- subprojects/test-runner/test-inhouse-runner/src/main/kotlin/com/avito/android/runner/annotation/Host.kt
+++
@@ -1,5 +0,0 @@
-package com.avito.android.runner.annotation
-
-@Retention(AnnotationRetention.RUNTIME)
-@Target(AnnotationTarget.CLASS, AnnotationTarget.FUNCTION)
-annotation class Host(val apiUrl: String)
