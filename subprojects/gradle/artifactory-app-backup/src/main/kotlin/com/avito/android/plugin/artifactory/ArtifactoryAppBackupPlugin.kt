924be316258b324f4b74d4dce50ba2df178425a7
Dmitriy Voronin <dsvoronin@gmail.com>
2021-06-07 17:29:40 +0300

    MBS-10881 artifactory-app-backup no longer applies maven-publish (#1050)
    it should be applied manually
    
    no longer works with gradle 7+ because "Cannot run Project.afterEvaluate(Action) when the project is already evaluated" (from maven publish plugin)

--- subprojects/gradle/artifactory-app-backup/src/main/kotlin/com/avito/android/plugin/artifactory/ArtifactoryAppBackupPlugin.kt
+++ subprojects/gradle/artifactory-app-backup/src/main/kotlin/com/avito/android/plugin/artifactory/ArtifactoryAppBackupPlugin.kt
@@ -1,15 +1,20 @@
 package com.avito.android.plugin.artifactory
 
 import com.android.build.gradle.internal.tasks.factory.dependsOn
+import com.avito.android.Problem
+import com.avito.android.asRuntimeException
 import com.avito.cd.buildOutput
 import com.avito.kotlin.dsl.getMandatoryStringProperty
+import com.avito.kotlin.dsl.withType
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.publish.PublishingExtension
 import org.gradle.api.publish.maven.MavenPublication
 import org.gradle.api.publish.maven.plugins.MavenPublishPlugin
 import org.gradle.api.publish.maven.tasks.PublishToMavenRepository
+import org.gradle.kotlin.dsl.findByType
 import org.gradle.kotlin.dsl.getByType
+import org.gradle.kotlin.dsl.withType
 import java.net.URI
 
 class ArtifactoryAppBackupPlugin : Plugin<Project> {
@@ -22,45 +27,72 @@ class ArtifactoryAppBackupPlugin : Plugin<Project> {
             project.objects
         )
 
-        project.extensions.configure<DefaultArtifactoryAppBackupExtension>(artifactoryBackupExtensionName) {
-            it.backups.all { backup ->
-                if (!project.plugins.hasPlugin(MavenPublishPlugin::class.java)) {
-                    applyMavenPublishPlugin(project)
+        project.plugins.withType<MavenPublishPlugin> {
+            configureMavenPublishPlugin(project)
+
+            project.extensions.configure<DefaultArtifactoryAppBackupExtension>(artifactoryBackupExtensionName) {
+                it.backups.all { backup ->
+                    project.createBackupPublication(backup)
+                    val publishTask = project.findMavenPublishTask(backup)
+                    if (publishTask != null) {
+                        publishTask.addSetArtifactsBuildOutputAction()
+                        @Suppress("DEPRECATION")
+                        project.tasks.artifactoryAppBackupTask().dependsOn(publishTask)
+                    } else {
+                        throw Problem(
+                            shortDescription = "Can't apply `com.avito.android.artifactory-app-backup` plugin",
+                            context = "ArtifactoryAppBackupPlugin modifying publication task",
+                            because = "This could happen if `maven-publish` plugin has breaking API changes, " +
+                                "or because of an error in `artifactory-app-backup` plugin itself",
+                            possibleSolutions = listOf(
+                                "Check `maven-publish` incompatible API changes " +
+                                    "and apply needed changes in ArtifactoryAppBackupPlugin",
+                                "Check recent changes in `artifactory-app-backup`"
+                            ),
+                            documentedAt = "https://docs.gradle.org/current/userguide/publishing_maven.html"
+                        ).asRuntimeException()
+                    }
                 }
-                project.createBackupPublication(backup)
-                val publishTask = project.findMavenPublishTask(backup)
-                publishTask.addSetArtifactsBuildOutputAction()
-                @Suppress("DEPRECATION")
-                project.tasks.artifactoryAppBackupTask().dependsOn(publishTask)
+            }
+        }
+
+        project.afterEvaluate {
+            if (!project.plugins.hasPlugin(MavenPublishPlugin::class.java)) {
+                throw Problem(
+                    shortDescription = "Can't apply `com.avito.android.artifactory-app-backup` plugin",
+                    context = "ArtifactoryAppBackupPlugin applying",
+                    because = "artifactory-app-backup has precondition: maven-publish plugin should be applied",
+                    possibleSolutions = listOf("Add `maven-publish` plugin to ${project.path}")
+                ).asRuntimeException()
             }
         }
     }
 
-    private fun Project.findMavenPublishTask(
-        backup: Backup
-    ): PublishToMavenRepository {
-        return tasks.withType(PublishToMavenRepository::class.java).matching {
-            it.repository.name == artifactoryRepositoryName &&
-                it.publication.name == backup.name
-        }.first()
+    private fun Project.findMavenPublishTask(backup: Backup): PublishToMavenRepository? {
+        return tasks.withType<PublishToMavenRepository>()
+            .matching {
+                it.repository.name == artifactoryRepositoryName && it.publication.name == backup.name
+            }
+            .firstOrNull()
     }
 
     private fun Project.createBackupPublication(backup: Backup) {
-        val publishing = extensions.getByType<PublishingExtension>()
-        val publication =
-            publishing.publications.create(backup.name, MavenPublication::class.java) { publication ->
+        val publishing = extensions.findByType<PublishingExtension>()
+
+        if (publishing != null) {
+            val publication = publishing.publications.create(backup.name, MavenPublication::class.java) { publication ->
                 publication.groupId = backup.name
                 publication.artifactId = backup.type
                 publication.version = backup.version
             }
 
-        backup.artifacts.forEach { (id, path) ->
-            publication.artifact(path) { it.classifier = id }
+            backup.artifacts.forEach { (id, path) ->
+                publication.artifact(path) { it.classifier = id }
+            }
         }
     }
 
-    private fun applyMavenPublishPlugin(project: Project) {
-        project.plugins.apply(MavenPublishPlugin::class.java)
+    private fun configureMavenPublishPlugin(project: Project) {
         val publishing = project.extensions.getByType<PublishingExtension>()
         val artifactoryUrl = project.getMandatoryStringProperty("artifactoryUrl").removeSuffix("/")
         val backupUrl = URI.create("$artifactoryUrl/apps-release-local/")
