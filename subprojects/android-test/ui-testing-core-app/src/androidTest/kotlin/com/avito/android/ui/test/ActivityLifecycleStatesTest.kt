b972d5117ab3aab33d5439731f44240fe1831e7d
Ruslan Mingaliev <mingaliev.rr@gmail.com>
2021-06-10 15:41:17 +0300

    MBS-9536 Add new test when activity is not launched

--- subprojects/android-test/ui-testing-core-app/src/androidTest/kotlin/com/avito/android/ui/test/ActivityLifecycleStatesTest.kt
+++ subprojects/android-test/ui-testing-core-app/src/androidTest/kotlin/com/avito/android/ui/test/ActivityLifecycleStatesTest.kt
@@ -3,7 +3,6 @@ package com.avito.android.ui.test
 import androidx.lifecycle.Lifecycle
 import com.avito.android.test.app.core.inHouseScreenRule
 import com.avito.android.ui.EmptyActivity
-import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import ru.avito.util.assertThrows
@@ -13,40 +12,48 @@ class ActivityLifecycleStatesTest {
     @get:Rule
     val activityRule = inHouseScreenRule<EmptyActivity>()
 
-    @Before
-    fun before() {
-        activityRule.launchActivity(null)
-    }
-
     @Test
     fun isDestroyed_succeed_whenActivityFinished() {
+        activityRule.launchActivity(null)
         activityRule.scenario.onActivity { it.finish() }
         activityRule.checks.isInState(Lifecycle.State.DESTROYED)
     }
 
     @Test
     fun isDestroyed_throwsAssertionError_whenActivityFinished() {
+        activityRule.launchActivity(null)
         activityRule.scenario.onActivity { it.finish() }
         assertThrows<AssertionError> { activityRule.checks.isNotInState(Lifecycle.State.DESTROYED) }
     }
 
     @Test
     fun isNotDestroyed_succeed_whenActivityLaunched() {
+        activityRule.launchActivity(null)
         activityRule.checks.isNotInState(Lifecycle.State.DESTROYED)
     }
 
     @Test
     fun isDestroyed_throwsAssertionError_whenActivityLaunched() {
+        activityRule.launchActivity(null)
         assertThrows<AssertionError> { activityRule.checks.isInState(Lifecycle.State.DESTROYED) }
     }
 
     @Test
     fun stateIsGreaterThanCreated_succeed_whenActivityLaunched() {
+        activityRule.launchActivity(null)
         activityRule.checks.isAtLeastInState(Lifecycle.State.CREATED)
     }
 
     @Test
     fun isResumed_succeed_whenActivityLaunched() {
+        activityRule.launchActivity(null)
         activityRule.checks.isInState(Lifecycle.State.RESUMED)
+    }
+
+    @Test
+    fun stateChecks_throwIllegalStateException_whenActivityIsNotLaunched() {
+        assertThrows<IllegalStateException> { activityRule.checks.isInState(Lifecycle.State.DESTROYED) }
+        assertThrows<IllegalStateException> { activityRule.checks.isInState(Lifecycle.State.INITIALIZED) }
+        activityRule.launchActivity(null) // or test will fail
     }
 }
