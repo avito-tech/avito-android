7c56165143d092469c5a45a0ccf694d1b8a94816
Dmitriy Voronin <dsvoronin@gmail.com>
2021-06-23 23:05:59 +0300

    MBS-10193 move gradleTestRuntime workaround to convention plugin (#1095)

--- build-logic/testing-convention/src/main/kotlin/convention.gradle-testing.gradle.kts
+++ build-logic/testing-convention/src/main/kotlin/convention.gradle-testing.gradle.kts
@@ -1,5 +1,7 @@
 @file:Suppress("UnstableApiUsage")
 
+import org.gradle.api.internal.classpath.ModuleRegistry
+import org.gradle.configurationcache.extensions.serviceOf
 import org.jetbrains.kotlin.gradle.dsl.KotlinJvmProjectExtension
 import org.jetbrains.kotlin.gradle.plugin.KotlinPluginWrapper
 
@@ -55,6 +57,13 @@ dependencies {
     "gradleTestRuntimeOnly"(libs.junitJupiterEngine)
     "gradleTestRuntimeOnly"(libs.junitPlatformRunner)
     "gradleTestRuntimeOnly"(libs.junitPlatformLauncher)
+
+    // workaround for https://github.com/gradle/gradle/issues/16774
+    "gradleTestRuntimeOnly"(
+        files(
+            serviceOf<ModuleRegistry>().getModule("gradle-tooling-api-builders").classpath.asFiles.first()
+        )
+    )
 }
 
 gradlePlugin {
