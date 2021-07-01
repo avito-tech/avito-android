00b61aa5adaad72419840be6671387db63a7053a
Sergey Boishtyan <ssboishtyan@avito.ru>
2021-07-01 16:43:14 +0300

    MBS-11503 Rename Ktulhu to Ctulhu

---
+++ subprojects/test-runner/report-viewer/src/main/kotlin/com/avito/report/internal/model/CtulhuTestCase.kt
@@ -0,0 +1,7 @@
+package com.avito.report.internal.model
+
+/**
+ * Ctulhu is an Avito's Test Model System
+ * prepared_data field. After analysis matches with TMS if has a test_case_id
+ */
+internal data class CtulhuTestCase(val description: String?)
--- subprojects/test-runner/report-viewer/src/main/kotlin/com/avito/report/internal/model/KtulhuTestCase.kt
+++
@@ -1,6 +0,0 @@
-package com.avito.report.internal.model
-
-/**
- * Прилетает в prepared_data после анализа совпадения с тестохранилкой если есть test_case_id
- */
-internal data class KtulhuTestCase(val description: String?)
