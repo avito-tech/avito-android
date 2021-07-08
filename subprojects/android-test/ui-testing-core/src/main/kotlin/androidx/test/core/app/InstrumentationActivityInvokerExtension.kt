e5bd6cb31020fc09039593dcda9c2d0bb143d58f
Ruslan Mingaliev <mingaliev.rr@gmail.com>
2021-07-08 21:20:16 +0300

    MBS-11598 Make `hasInstrumentationActivityComponent` internal

--- subprojects/android-test/ui-testing-core/src/main/kotlin/androidx/test/core/app/InstrumentationActivityInvokerExtension.kt
+++ subprojects/android-test/ui-testing-core/src/main/kotlin/androidx/test/core/app/InstrumentationActivityInvokerExtension.kt
@@ -5,7 +5,7 @@ import android.content.Intent
 /**
  * TODO: Remove after MBS-11523
  */
-fun Intent.hasInstrumentationActivityComponent(): Boolean {
+internal fun Intent.hasInstrumentationActivityComponent(): Boolean {
     val instrumentationComponents: List<String> = listOf(
         InstrumentationActivityInvoker.EmptyActivity::class.java.name,
         InstrumentationActivityInvoker.EmptyFloatingActivity::class.java.name,
