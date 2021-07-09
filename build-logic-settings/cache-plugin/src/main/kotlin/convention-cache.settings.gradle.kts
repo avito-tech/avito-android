12ffeb25ccd78e85b6beac22fc32faac8ca04ebb
Dmitriy Voronin <dsvoronin@gmail.com>
2021-07-09 14:29:18 +0300

    MBS-10881 pr leftovers (#1137)

--- build-logic-settings/cache-plugin/src/main/kotlin/convention-cache.settings.gradle.kts
+++ build-logic-settings/cache-plugin/src/main/kotlin/convention-cache.settings.gradle.kts
@@ -1,8 +1,3 @@
-import org.gradle.api.provider.Provider
-import org.gradle.caching.http.HttpBuildCache
-import org.gradle.kotlin.dsl.extra
-import org.gradle.kotlin.dsl.remote
-
 @Suppress("UnstableApiUsage")
 val avitoGithubRemoteCacheHost: Provider<String> = settings.providers
     .environmentVariable("GRADLE_CACHE_NODE_HOST")
@@ -11,14 +6,6 @@ val avitoGithubRemoteCacheHost: Provider<String> = settings.providers
 val avitoGithubRemoteCachePush: String =
     extra.properties.getOrDefault("avitoGithub.gradle.buildCache.remote.push", "false").toString()
 
-/**
- * Included builds will inherit this cache config
- * https://docs.gradle.org/current/userguide/build_cache.html#sec:build_cache_composite
- *
- * TODO it is not working as expected with --project-dir subprojects
- *  problem is that's how IDE runs tasks from included builds by default
- *  enabled only for "subprojects" for now
- */
 buildCache {
     remote<HttpBuildCache> {
         setUrl("http://${avitoGithubRemoteCacheHost.orNull}/cache/")
