ecef123c7e529d300c60517a1e9dc5c8e1825b7f
Dmitriy Voronin <dsvoronin@gmail.com>
2021-08-30 16:46:05 +0300

    MBS-11822 fix local report link (#1189)
    look at deviceName, not buildId to determine if it's a local run

--- subprojects/gradle/build-environment/src/main/kotlin/com/avito/utils/gradle/EnvArgsImpl.kt
+++ subprojects/gradle/build-environment/src/main/kotlin/com/avito/utils/gradle/EnvArgsImpl.kt
@@ -8,26 +8,24 @@ import java.io.Serializable
 
 internal class EnvArgsImpl(project: Project) : EnvArgs, Serializable {
 
-    override val build: EnvArgs.Build
-
-    init {
-        // todo make local by default?
-        build = when (project.getOptionalStringProperty("avito.build", "teamcity")) {
-            "teamcity" -> {
-                val teamcityBuildId = project.getMandatoryIntProperty("teamcityBuildId")
-                EnvArgs.Build.Teamcity(
-                    id = teamcityBuildId,
-                    url = project.getMandatoryStringProperty("teamcityUrl") +
-                            "/viewLog.html?buildId=$teamcityBuildId&tab=buildLog",
-                    number = project.getMandatoryStringProperty("buildNumber"),
-                    type = "teamcity-${project.getMandatoryStringProperty("teamcityBuildType")}"
-                )
-            }
-            "local" ->
-                EnvArgs.Build.Local(EnvArgs.Build.Local.Id.FOR_LOCAL_KUBERNETES_RUN)
-
-            else ->
-                throw IllegalStateException("property avito.build must be 'teamcity' or 'local'")
+    /**
+     * todo make local by default?
+     */
+    override val build: EnvArgs.Build = when (project.getOptionalStringProperty("avito.build", "teamcity")) {
+        "teamcity" -> {
+            val teamcityBuildId = project.getMandatoryIntProperty("teamcityBuildId")
+            EnvArgs.Build.Teamcity(
+                id = teamcityBuildId,
+                url = project.getMandatoryStringProperty("teamcityUrl") +
+                        "/viewLog.html?buildId=$teamcityBuildId&tab=buildLog",
+                number = project.getMandatoryStringProperty("buildNumber"),
+                type = "teamcity-${project.getMandatoryStringProperty("teamcityBuildType")}"
+            )
         }
+        "local" ->
+            EnvArgs.Build.Local(EnvArgs.Build.Local.Id.FOR_LOCAL_KUBERNETES_RUN)
+
+        else ->
+            throw IllegalStateException("property avito.build must be 'teamcity' or 'local'")
     }
 }
