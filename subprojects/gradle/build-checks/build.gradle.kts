7449575cdbc52865c22532bc4c12a91641e90690
Dmitriy Voronin <dsvoronin@gmail.com>
2021-09-02 00:14:07 +0300

    MBS-10898 remove room and robolectric convention plugins (#1195)

--- subprojects/gradle/build-checks/build.gradle.kts
+++ subprojects/gradle/build-checks/build.gradle.kts
@@ -20,7 +20,6 @@ dependencies {
 
     // for test
     implementation(projects.gradle.moduleTypes)
-    implementation(projects.gradle.roomConfig)
 
     gradleTestImplementation(projects.gradle.testProject)
 }
