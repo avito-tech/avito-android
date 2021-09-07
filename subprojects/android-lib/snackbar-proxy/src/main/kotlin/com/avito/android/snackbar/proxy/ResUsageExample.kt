67efbcd287de27bf0932931c1c61f693cb737b4c
Evgeniy Krivobokov <ekrivobokov@avito.ru>
2021-09-07 09:50:20 +0300

    MBS-11417 Update dependency-analysis plugin

--- subprojects/android-lib/snackbar-proxy/src/main/kotlin/com/avito/android/snackbar/proxy/ResUsageExample.kt
+++ subprojects/android-lib/snackbar-proxy/src/main/kotlin/com/avito/android/snackbar/proxy/ResUsageExample.kt
@@ -2,6 +2,8 @@ package com.avito.android.snackbar.proxy
 
 class ResUsageExample {
 
-    // This usage is detected
+    // This usage is detected if uncomment
 //    val usageFromCodeFromAnotherModule: Int = com.avito.android.ui.R.attr.themeColor
+
+    // See res/drawable/ic_pin.xml for non-detected resource usage
 }
