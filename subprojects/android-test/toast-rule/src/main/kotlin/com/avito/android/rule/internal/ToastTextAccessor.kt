e6693b677a6d39b2b4bdfafed023a64895e03d91
Evgeniy Krivobokov <ekrivobokov@avito.ru>
2021-09-14 00:55:01 +0300

    MBS-11882 Fix ToastRule in Android 11 target

---
+++ subprojects/android-test/toast-rule/src/main/kotlin/com/avito/android/rule/internal/ToastTextAccessor.kt
@@ -0,0 +1,74 @@
+package com.avito.android.rule.internal
+
+import android.os.Build
+import android.widget.TextView
+import android.widget.Toast
+import androidx.test.platform.app.InstrumentationRegistry
+import com.avito.android.test.util.HiddenApiOpener
+
+/**
+ * Reads text from a Toast.
+ * See API-specific implementations.
+ */
+internal interface ToastTextAccessor {
+
+    /**
+     * Reads text from Toast created by [Toast.makeText].
+     * Returns null in case of custom view
+     */
+    fun text(toast: Toast): String?
+
+    companion object {
+
+        fun create(): ToastTextAccessor {
+            val inAppToasts = Build.VERSION.SDK_INT < 30 ||
+                InstrumentationRegistry.getInstrumentation().targetContext.applicationInfo.targetSdkVersion < 30
+
+            return if (inAppToasts) {
+                InAppToastTextAccessor()
+            } else {
+                SystemUIToastTextAccessor()
+            }
+        }
+    }
+}
+
+/**
+ * Before Android 11 toasts were shown inside application.
+ * [Toast.makeText] inflated View and assigned it as [Toast.setView]
+ *
+ * See https://developer.android.com/about/versions/11/behavior-changes-11#toasts
+ */
+internal class InAppToastTextAccessor : ToastTextAccessor {
+
+    override fun text(toast: Toast): String? {
+        @Suppress("DEPRECATION")
+        val view = requireNotNull(toast.view) {
+            "Toast.view must be present in $toast"
+        }
+        val defaultTextView = view.findViewById<TextView>(android.R.id.message)
+        return defaultTextView?.text?.toString()
+    }
+}
+
+/**
+ * Starting from Android 11 toasts are shown in system service.
+ * [Toast.makeText] doesn't inflate View and stores only text.
+ *
+ * See https://developer.android.com/about/versions/11/behavior-changes-11#toasts
+ */
+internal class SystemUIToastTextAccessor : ToastTextAccessor {
+
+    override fun text(toast: Toast): String? {
+        val textField = toast.getFieldByReflection("mText") as CharSequence?
+        return textField?.toString()
+    }
+}
+
+internal fun Any.getFieldByReflection(fieldName: String): Any {
+    HiddenApiOpener.ensureUnseal()
+
+    return this::class.java.getDeclaredField(fieldName)
+        .also { it.isAccessible = true }
+        .get(this)!!
+}
--- subprojects/android-test/ui-testing-core-app/src/main/kotlin/com/avito/android/ui/ToastActivity.kt
+++ subprojects/android-test/ui-testing-core-app/src/main/kotlin/com/avito/android/ui/ToastActivity.kt
@@ -1,7 +1,15 @@
 package com.avito.android.ui
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
+import android.view.Gravity
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.avito.android.util.ProxyToast
 import com.avito.android.util.showToast
 
 class ToastActivity : AppCompatActivity() {
@@ -10,6 +18,49 @@ class ToastActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_empty)
 
-        showToast("I'am a toast!")
+        findViewById<View>(R.id.root).post {
+            setupSimpleToast()
+            setupCustomToast()
+        }
+    }
+
+    private fun setupCustomToast() {
+        if (intent.getBooleanExtra(EXTRA_SHOW_CUSTOM_TOAST, false)) {
+            val layout: View = layoutInflater.inflate(
+                R.layout.custom_toast,
+                findViewById<View>(R.id.root) as ViewGroup,
+                false
+            )
+            @SuppressLint("SetTextI18n")
+            layout.findViewById<TextView>(R.id.text).text = "Custom toast"
+
+            val toast = Toast(applicationContext)
+            toast.setGravity(Gravity.CENTER_VERTICAL, 0, 0)
+            toast.duration = Toast.LENGTH_LONG
+            @Suppress("DEPRECATION")
+            toast.view = layout
+
+            ProxyToast.instance.show(toast)
+        }
+    }
+
+    private fun setupSimpleToast() {
+        if (intent.getBooleanExtra(EXTRA_SHOW_SIMPLE_TOAST, false)) {
+            showToast("Simple toast")
+        }
+    }
+
+    companion object {
+
+        private const val EXTRA_SHOW_SIMPLE_TOAST = "extra_simple_toast"
+        private const val EXTRA_SHOW_CUSTOM_TOAST = "extra_custom_toast"
+
+        fun intent(
+            showSimpleToast: Boolean = false,
+            showCustomToast: Boolean = false,
+        ): (Intent) -> Intent = {
+            it.putExtra(EXTRA_SHOW_SIMPLE_TOAST, showSimpleToast)
+            it.putExtra(EXTRA_SHOW_CUSTOM_TOAST, showCustomToast)
+        }
     }
 }
