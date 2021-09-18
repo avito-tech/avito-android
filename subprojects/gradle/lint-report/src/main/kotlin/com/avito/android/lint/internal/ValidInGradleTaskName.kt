20cce33cf54ab4fb1e4f635119405efdc1281539
Dmitriy Voronin <dsvoronin@gmail.com>
2021-09-18 15:43:06 +0300

    MBS-11865 remove lint-report module and corresponding ciSteps check (#1216)
    * MBS-11865 remove lint-report module and corresponding ciSteps check
    
    * Update docs/content/blog/2020/02/lint-worker-api-workaround.md
    
    Co-authored-by: Eugene Krivobokov <eugene.krivobokov@gmail.com>
    
    Co-authored-by: Eugene Krivobokov <eugene.krivobokov@gmail.com>

--- subprojects/gradle/lint-report/src/main/kotlin/com/avito/android/lint/internal/ValidInGradleTaskName.kt
+++
@@ -1,8 +0,0 @@
-package com.avito.android.lint.internal
-
-import com.avito.capitalize
-
-private val noSymbols = Regex("[\\W]+")
-
-internal fun String.validInGradleTaskName(): String = split(noSymbols)
-    .joinToString(separator = "") { it.capitalize() }
