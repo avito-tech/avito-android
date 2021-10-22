9c867ca08e4bf8e9b2915757cec5c73efbbe77f8
Ruslan Mingaliev <mingaliev.rr@gmail.com>
2021-10-22 23:24:42 +0300

    MA-2182 Remove deprecated properties from code ownership plugin

--- subprojects/gradle/code-ownership/src/main/kotlin/Visibility.kt
+++
@@ -1,6 +0,0 @@
-@Deprecated("Modules visibility restriction is deprecated. Left for backward compatibility")
-public enum class Visibility {
-    PRIVATE,
-    TEAM,
-    PUBLIC
-}
--- subprojects/gradle/code-ownership/src/main/kotlin/com/avito/android/CodeOwnershipExtension.kt
+++ subprojects/gradle/code-ownership/src/main/kotlin/com/avito/android/CodeOwnershipExtension.kt
@@ -1,19 +1,11 @@
-@file:Suppress("deprecation")
 package com.avito.android
 
-import Visibility
 import com.avito.android.model.Owner
 import org.gradle.api.plugins.ExtensionContainer
 import org.gradle.kotlin.dsl.findByType
 
 public open class CodeOwnershipExtension(
     public var owners: Set<Owner> = emptySet(),
-    @Deprecated("Modules visibility restriction is deprecated. Use `owners` property instead")
-    public var team: String? = null,
-    @Deprecated("Modules visibility restriction is deprecated.")
-    public var visibility: Visibility = Visibility.PUBLIC,
-    @Deprecated("Modules visibility restriction is deprecated.")
-    public var allowedDependencies: Set<String> = emptySet()
 ) {
     public fun checkProjectOwnershipSettings(projectPath: String) {
         if (owners.isEmpty()) {
