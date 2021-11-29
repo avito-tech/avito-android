4c5e7eeb3fd4260482c9292f3a09748dbcf3c1bb
Eugene Krivobokov <eugene.krivobokov@gmail.com>
2021-11-29 21:46:04 +0300

    MBS-12266 Make module type mandatory (#1294)

--- subprojects/gradle/module-types/src/main/kotlin/com/avito/android/module_type/ModuleTypeExtension.kt
+++ subprojects/gradle/module-types/src/main/kotlin/com/avito/android/module_type/ModuleTypeExtension.kt
@@ -5,4 +5,17 @@ import org.gradle.api.provider.Property
 public abstract class ModuleTypeExtension {
 
     public abstract val type: Property<ModuleType>
+
+    internal fun ensureHasType(projectPath: String) {
+        check(type.isPresent) {
+            """
+            |Module type must be set for the $projectPath project.
+            |Configure an extension in the buildscript: 
+            |
+            |module {
+            |   type.set(...)
+            |}
+            """.trimMargin()
+        }
+    }
 }
