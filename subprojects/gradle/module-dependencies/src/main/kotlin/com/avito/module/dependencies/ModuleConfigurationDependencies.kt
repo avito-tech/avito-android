cfb0f9a9cf1a9273c813a3d8b3ca06ac9de9d8a3
Eugene Krivobokov <eugene.krivobokov@gmail.com>
2021-12-16 13:38:42 +0300

    MBS-12444 Fix missing configurations in module dependencies (#1304)

--- subprojects/gradle/module-dependencies/src/main/kotlin/com/avito/module/dependencies/ModuleConfigurationDependencies.kt
+++ subprojects/gradle/module-dependencies/src/main/kotlin/com/avito/module/dependencies/ModuleConfigurationDependencies.kt
@@ -2,23 +2,28 @@ package com.avito.module.dependencies
 
 import com.avito.module.configurations.ConfigurationType
 import org.gradle.api.Project
+import org.gradle.api.artifacts.Configuration
 import org.gradle.api.artifacts.ProjectDependency
 
 public fun Project.directDependenciesOnProjects(
     types: Set<ConfigurationType>
 ): Map<ConfigurationType, Set<Project>> {
-    return configurations
-        .associateWith {
-            it.dependencies
-                .matching { it is ProjectDependency }
-                .map { (it as ProjectDependency).dependencyProject }
-                // project has dependency to itself in a default configuration
-                .filter { it != this }
+    val project = this
+
+    return configurations.groupBy { ConfigurationType.of(it) }
+        .filterKeys { configurationType -> types.contains(configurationType) }
+        .mapValues { (_, configurations) ->
+            configurations
+                .flatMap { directProjectDependencies(project, it) }
                 .toSet()
         }
-        .filterValues { it.isNotEmpty() }
-        .mapKeys { ConfigurationType.of(it.key) }
-        .filterKeys { type ->
-            types.contains(type)
-        }
+}
+
+private fun directProjectDependencies(project: Project, configuration: Configuration): Set<Project> {
+    return configuration.dependencies
+        .matching { it is ProjectDependency }
+        .map { (it as ProjectDependency).dependencyProject }
+        // project has dependency to itself in a default configuration
+        .filter { it != project }
+        .toSet()
 }
