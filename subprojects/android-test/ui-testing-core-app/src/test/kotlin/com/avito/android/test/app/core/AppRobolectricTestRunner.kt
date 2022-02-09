add9027f76ad4ab78ba8e0d2c4f9451d422513c4
Ruslan Mingaliev <mingaliev.rr@gmail.com>
2022-02-09 18:40:33 +0300

    MBS-12648 Remove AppRobolectricTestRunner

--- subprojects/android-test/ui-testing-core-app/src/test/kotlin/com/avito/android/test/app/core/AppRobolectricTestRunner.kt
+++
@@ -1,5 +0,0 @@
-package com.avito.android.test.app.core
-
-import com.avito.robolectric.runner.InHouseRobolectricTestRunner
-
-open class AppRobolectricTestRunner(testClass: Class<*>) : InHouseRobolectricTestRunner(testClass)
