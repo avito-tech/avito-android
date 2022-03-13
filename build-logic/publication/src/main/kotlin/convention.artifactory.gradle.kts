73a54378e6b2a10b0d925825d0abd8d6ed43a517
Dmitriy Voronin <dvoronin@avito.ru>
2022-03-13 23:01:30 +0300

    MBS-12826 artifactory publish
    `./gradlew publishToArtifactory` - to publish locally

---
+++ build-logic/publication/src/main/kotlin/convention.artifactory.gradle.kts
@@ -0,0 +1,35 @@
+plugins {
+    id("convention.publish-base")
+}
+
+val artifactoryUrlProperty: Provider<String> = providers.gradleProperty("artifactoryUrl")
+
+val artifactoryUser: Provider<String> = providers.gradleProperty("avito.artifactory.user")
+
+val artifactoryPassword: Provider<String> = providers.gradleProperty("avito.artifactory.password")
+
+val repoName = "Artifactory"
+
+val publishTask = tasks.register<Task>("publishToArtifactory") {
+    group = "publication"
+
+    dependsOn(tasks.named("publishAllPublicationsTo${repoName}Repository"))
+}
+
+publishing {
+    repositories {
+        maven {
+            name = repoName
+            val artifactoryUrl = artifactoryUrlProperty.orNull
+            if (!artifactoryUrl.isNullOrBlank()) {
+                setUrl("$artifactoryUrl/libs-release-local")
+            }
+            credentials {
+                username = artifactoryUser.orNull
+                password = artifactoryPassword.orNull
+            }
+
+            isAllowInsecureProtocol = true
+        }
+    }
+}
---
+++ ci/publish_artifactory.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+# This is an entrypoint for CI build step, don't change it's relative path (name)
+
+source $(dirname $0)/_main.sh
+
+# force Envs to be set up
+set -uex
+
+function publish() {
+    local readonly user="-Pavito.artifactory.user=${ARTIFACTORY_PUBLISH_USER}"
+    local readonly password="-Pavito.artifactory.password=${ARTIFACTORY_PUBLISH_PASSWORD}"
+
+    local readonly CREDENTIALS="${user} ${password}"
+
+    runInBuilder "./gradlew publishRelease ${GRADLE_ARGS} ${CREDENTIALS} --no-parallel --stacktrace"
+}
+
+readonly CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
+
+if [ "$CURRENT_BRANCH" == "develop" ]; then
+    echo "ERROR: Releasing from develop branch. It can clash with a release from a release branch. Develop contains the next version. See a release process in docs."
+    exit 1
+fi
+
+publish
