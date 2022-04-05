06a13e5b12a3c0067065e7e8cf2341fa352a3039
Sergey Boishtyan <vitilus123@gmail.com>
2022-04-05 13:30:50 +0300

    MBSA-481 Add avd options description

--- subprojects/emcee/android-device/src/main/kotlin/com/avito/android/device/avd/internal/StartAvd.kt
+++ subprojects/emcee/android-device/src/main/kotlin/com/avito/android/device/avd/internal/StartAvd.kt
@@ -18,6 +18,10 @@ internal class StartAvd(
         val avdConfig = configurationProvider.provide(ConfigurationKey(sdk, type))
         return CommandLine.create(
             command = androidSdk.resolveSibling("emulator").absolutePathString(),
+            /**
+             * Official startup options documentation:
+             * https://developer.android.com/studio/run/emulator-commandline#startup-options
+             */
             args = listOf(
                 "-avd ${avdConfig.emulatorFileName}",
                 "-sdcard ${avdConfig.sdCardFileName}",
@@ -25,7 +29,13 @@ internal class StartAvd(
                 "-no-snapshot",
                 "-no-boot-anim",
                 "-no-audio",
+                /**
+                 * Emulator will have 2Gb on the disk. Default is 256Mb
+                 */
                 "-partition-size 2048",
+                /**
+                 * Use CPU as GPU
+                 */
                 "-gpu swiftshader_indirect",
                 "-verbose"
             )
