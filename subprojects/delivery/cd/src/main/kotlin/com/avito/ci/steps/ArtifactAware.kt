b6e45847db12934afd4e1e571227fbb9a34936cb
Dmitriy Voronin <dvoronin@avito.ru>
2022-07-12 13:57:46 +0300

    MBSA-54 remove unused modules

--- subprojects/delivery/cd/src/main/kotlin/com/avito/ci/steps/ArtifactAware.kt
+++
@@ -1,6 +0,0 @@
-package com.avito.ci.steps
-
-public interface ArtifactAware {
-
-    public var artifacts: Set<String>
-}
--- subprojects/delivery/cd/src/main/kotlin/com/avito/ci/steps/ArtifactAwareImpl.kt
+++
@@ -1,6 +0,0 @@
-package com.avito.ci.steps
-
-internal class ArtifactAwareImpl : ArtifactAware {
-
-    override var artifacts: Set<String> = mutableSetOf()
-}
