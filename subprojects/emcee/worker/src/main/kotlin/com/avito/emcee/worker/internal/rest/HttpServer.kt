c1eecc7298b3f7e6816c47eac891f3511376cd1f
Dmitriy Voronin <dvoronin@avito.ru>
2022-07-13 21:28:44 +0300

    MBSA-483 fix flaky httpserver test

--- subprojects/emcee/worker/src/main/kotlin/com/avito/emcee/worker/internal/rest/HttpServer.kt
+++ subprojects/emcee/worker/src/main/kotlin/com/avito/emcee/worker/internal/rest/HttpServer.kt
@@ -5,66 +5,81 @@ import java.io.BufferedReader
 import java.io.InputStreamReader
 import java.io.PrintWriter
 import java.net.ServerSocket
+import java.net.Socket
 
-internal class HttpServer(private val handlers: List<RequestHandler>, private val debug: Boolean) {
+internal class HttpServer(
+    private val handlers: List<RequestHandler>,
+    private val debug: Boolean
+) {
 
-    private var serverThread: Thread? = null
+    private var serverSocket: ServerSocket? = null
 
     @Volatile
-    private var isStopped = false
+    internal var isStarted: Boolean = false
 
     fun start(port: Int) {
-        serverThread = Thread {
-            val ss = ServerSocket(port)
-            if (debug) println("Starting REST server on $port port/")
-
-            ss.use {
-                while (!Thread.interrupted()) {
-                    val connection = ss.accept()
-                    val reader = BufferedReader(InputStreamReader(connection.getInputStream()))
-                    val writer = PrintWriter(connection.getOutputStream())
-
-                    try {
-                        val request = reader.readLine() ?: continue
-
-                        val body = StringBuilder()
-                        while (true) {
-                            val line = reader.readLine()
-                            if (line == null || line.isEmpty()) break
-                            body.append(line).append("\n")
-                        }
-
-                        if (debug) println("<-- $request\n$body")
-
-                        val handler = findHandler(request)
-                        val response = if (handler != null) {
-                            handler.response().wrapToSuccessfulResponse()
-                        } else {
-                            badRequest()
-                        }
-                        if (debug) println("--> $response")
-
-                        writer.write(response)
-                    } finally {
-                        writer.close()
-                        reader.close()
-                        connection.close()
-                    }
-                }
-            }
-        }.apply { start() }
+        if (debug) println("Starting REST server on $port port/")
+        serverSocket = ServerSocket(port)
+        isStarted = true
+        while (true) {
+            ClientHandler(
+                clientSocket = serverSocket!!.accept(),
+                debug = debug,
+                handlers = handlers
+            ).start()
+        }
     }
 
     fun stop() {
-        require(!isStopped) { "Server has not been started" }
-        isStopped = true
+        serverSocket?.close()
+        isStarted = false
     }
 
-    private fun findHandler(request: String): RequestHandler? {
-        val parts = request.split(" ")
-        val method = HttpMethod.valueOf(parts[0])
-        val path = parts[1]
-        return handlers.find { it.method == method && it.path == path }
+    @Suppress("IfThenToElvis")
+    private class ClientHandler(
+        private val clientSocket: Socket,
+        private val debug: Boolean,
+        private val handlers: List<RequestHandler>
+    ) : Thread() {
+
+        override fun run() {
+            val reader = BufferedReader(InputStreamReader(clientSocket.getInputStream()))
+            val writer = PrintWriter(clientSocket.getOutputStream())
+
+            try {
+                val request = reader.readLine() ?: return
+
+                val body = StringBuilder()
+                while (true) {
+                    val line = reader.readLine()
+                    if (line == null || line.isEmpty()) break
+                    body.append(line).append("\n")
+                }
+
+                if (debug) println("<-- $request\n$body")
+
+                val handler = findHandler(request)
+                val response = if (handler != null) {
+                    handler.response().wrapToSuccessfulResponse()
+                } else {
+                    badRequest()
+                }
+                if (debug) println("--> $response")
+
+                writer.write(response)
+            } finally {
+                writer.close()
+                reader.close()
+                clientSocket.close()
+            }
+        }
+
+        private fun findHandler(request: String): RequestHandler? {
+            val parts = request.split(" ")
+            val method = HttpMethod.valueOf(parts[0])
+            val path = parts[1]
+            return handlers.find { it.method == method && it.path == path }
+        }
     }
 
     class Builder {
