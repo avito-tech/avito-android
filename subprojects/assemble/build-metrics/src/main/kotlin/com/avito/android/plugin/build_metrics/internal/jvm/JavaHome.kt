68d13715ef81ab46cc5c27925dff0a128630c0eb
Eugene Krivobokov <ekrivobokov@avito.ru>
2022-07-15 09:46:15 +0300

    Mbsa 648 Collect JVM metrics in parallel to avoid missing short-lived processes (#1368)

---
+++ subprojects/assemble/build-metrics/src/main/kotlin/com/avito/android/plugin/build_metrics/internal/jvm/JavaHome.kt
@@ -0,0 +1,11 @@
+package com.avito.android.plugin.build_metrics.internal.jvm
+
+import java.io.File
+
+internal class JavaHome(
+    val path: File = File(System.getProperty("java.home"))
+) {
+    val isJdk: Boolean by lazy {
+        File(path, "bin/jps").exists()
+    }
+}
