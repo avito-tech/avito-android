05c9114d1d439ede7a042436ce069049b99d05c0
Evgeniy Krivobokov <ekrivobokov@avito.ru>
2022-08-03 15:24:31 +0300

    MBSA-686 Use 32 build tools in android-builder image

--- ci/docker/android-builder/unzip_from_url.sh
+++ ci/docker/android-builder/unzip_from_url.sh
@@ -2,7 +2,11 @@
 
 set -e
 
-TEMP_ZIP_FILE=temp.zip
+# Why do we use a file instead of pipe?
+# - unzip doesn't support reading from pipe
+# - jar does, but it doesn't validate input zip.
+#   In case of missing or invalid file it won't fail.
+TEMP_ZIP_FILE=$(mktemp)
 
 curl "$1" --progress-bar --location --output $TEMP_ZIP_FILE &&
     unzip $TEMP_ZIP_FILE -d "$2" &&
