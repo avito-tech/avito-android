dc760aeaa933fdde8b7ea0f373f70faf681a3903
Sergey Boishtyan <vitilus123@gmail.com>
2022-09-28 18:21:23 +0300

    MBSA-743 Extract nupokati v2 logic configuration (#1413)

--- subprojects/delivery/nupokati/src/main/kotlin/com/avito/android/NupokatiExtension.kt
+++ subprojects/delivery/nupokati/src/main/kotlin/com/avito/android/NupokatiExtension.kt
@@ -1,35 +1,8 @@
 package com.avito.android
 
-import com.avito.android.model.input.CdBuildConfigParserFactory
-import org.gradle.api.Action
-import org.gradle.api.file.RegularFileProperty
 import org.gradle.api.provider.Property
-import org.gradle.api.provider.Provider
-import org.gradle.api.tasks.Nested
 
-public abstract class NupokatiExtension {
-
-    @get:Nested
-    public abstract val artifactory: ArtifactoryExtension
-
-    @get:Nested
-    public abstract val reportViewer: ReportViewerExtension
-
-    public abstract val cdBuildConfigFile: RegularFileProperty
-
-    public abstract val teamcityBuildUrl: Property<String>
+public abstract class NupokatiExtension : BaseNupokatiExtension() {
 
     public abstract val releaseBuildVariantName: Property<String>
-
-    public fun artifactory(action: Action<ArtifactoryExtension>) {
-        action.execute(artifactory)
-    }
-
-    public fun reportViewer(action: Action<ReportViewerExtension>) {
-        action.execute(reportViewer)
-    }
-
-    public fun releaseVersion(): Provider<String> {
-        return cdBuildConfigFile.map(CdBuildConfigParserFactory()).map { it.releaseVersion }
-    }
 }
