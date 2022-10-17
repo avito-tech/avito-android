102e7f554e9394ea21b95110cfc1a00fea89cc70
Sergey Boishtyan <vitilus123@gmail.com>
2022-10-17 23:36:12 +0600

    MBSA-769 Configure java.util.Logging format

--- ci/_environment.sh
+++ ci/_environment.sh
@@ -12,5 +12,5 @@ if [[ -z "${ARTIFACTORY_URL+x}" ]]; then
 fi
 
 IMAGE_ANDROID_BUILDER=${DOCKER_REGISTRY}/android/builder-hermetic:29c8300ac600
-IMAGE_BUILDER=${DOCKER_REGISTRY}/android/image-builder:10a9bea84f83
+IMAGE_BUILDER=${DOCKER_REGISTRY}/android/image-builder:44a68fd26323
 DOCUMENTATION_IMAGE=${DOCKER_REGISTRY}/android/documentation:802502572f
--- ci/docker/image-builder/src/main/kotlin/ru/avito/image_builder/Main.kt
+++ ci/docker/image-builder/src/main/kotlin/ru/avito/image_builder/Main.kt
@@ -6,12 +6,36 @@ import ru.avito.image_builder.internal.cli.BuildImage
 import ru.avito.image_builder.internal.cli.PublishEmceeWorker
 import ru.avito.image_builder.internal.cli.PublishEmulator
 import ru.avito.image_builder.internal.cli.PublishImage
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
+import java.util.logging.Level
+import java.util.logging.LogRecord
+import java.util.logging.Logger
+import java.util.logging.SimpleFormatter
 
 public object Main {
 
     @OptIn(ExperimentalCli::class)
     @JvmStatic
     public fun main(args: Array<String>) {
+        Logger.getLogger("").apply {
+            level = Level.INFO
+            for (handler in handlers) {
+                level = Level.INFO
+                // Sets up logging format similar to Emcee queue
+                handler.formatter = object : SimpleFormatter() {
+
+                    // 2022-11-28 16:05:30.500
+                    val dateFormatter = SimpleDateFormat("yyyy-MM-dd kk:mm:ss.SSS", Locale.ROOT)
+
+                    override fun format(record: LogRecord): String {
+                        // [INFO] 2022-11-25 16:05:30.500: Message
+                        return "[${record.level}] ${dateFormatter.format(Date(record.millis))}: ${record.message}\n"
+                    }
+                }
+            }
+        }
         val parser = ArgParser(
             programName = "image-builder",
         )
