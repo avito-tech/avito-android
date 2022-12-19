a1d5c61ba192009e3b02598acc52612a7e51b8c6
Mikhail Zinchenko <mvzinchenko@avito.ru>
2022-12-19 15:35:42 +0100

    MA-2884 Fix ab and ft configuration applying without a root

--- subprojects/gradle/tech-budget/src/main/kotlin/com/avito/android/tech_budget/internal/ab_tests/ABTestsConfigurator.kt
+++ subprojects/gradle/tech-budget/src/main/kotlin/com/avito/android/tech_budget/internal/ab_tests/ABTestsConfigurator.kt
@@ -6,6 +6,7 @@ import com.avito.android.tech_budget.ab_tests.CollectProjectABTestsTask
 import com.avito.android.tech_budget.internal.TechBudgetConfigurator
 import com.avito.android.tech_budget.internal.owners.requireCodeOwnershipExtension
 import com.avito.android.tech_budget.internal.utils.parser.JsonFileParser
+import com.avito.kotlin.dsl.isRoot
 import com.avito.kotlin.dsl.typedNamed
 import org.gradle.api.Project
 import org.gradle.kotlin.dsl.getByType
@@ -14,6 +15,8 @@ import org.gradle.kotlin.dsl.register
 internal class ABTestsConfigurator : TechBudgetConfigurator {
 
     override fun configure(project: Project) {
+        if (!project.isRoot()) return // Collection is enabled only for root
+
         project.tasks.register<UploadABTestsTask>(UploadABTestsTask.NAME) {
             val techBudgetExtension = project.extensions.getByType<TechBudgetExtension>()
             val collectProjectABTestsTask = project.tasks.typedNamed<CollectProjectABTestsTask>(
--- subprojects/gradle/tech-budget/src/main/kotlin/com/avito/android/tech_budget/internal/feature_toggles/FeatureTogglesConfigurator.kt
+++ subprojects/gradle/tech-budget/src/main/kotlin/com/avito/android/tech_budget/internal/feature_toggles/FeatureTogglesConfigurator.kt
@@ -6,6 +6,7 @@ import com.avito.android.tech_budget.feature_toggles.FeatureToggle
 import com.avito.android.tech_budget.internal.TechBudgetConfigurator
 import com.avito.android.tech_budget.internal.owners.requireCodeOwnershipExtension
 import com.avito.android.tech_budget.internal.utils.parser.JsonFileParser
+import com.avito.kotlin.dsl.isRoot
 import com.avito.kotlin.dsl.typedNamed
 import org.gradle.api.Project
 import org.gradle.kotlin.dsl.getByType
@@ -14,6 +15,8 @@ import org.gradle.kotlin.dsl.register
 internal class FeatureTogglesConfigurator : TechBudgetConfigurator {
 
     override fun configure(project: Project) {
+        if (!project.isRoot()) return // Collection is enabled only for root
+
         project.tasks.register<UploadFeatureTogglesTask>(UploadFeatureTogglesTask.NAME) {
             val techBudgetExtension = project.extensions.getByType<TechBudgetExtension>()
             val collectProjectFeatureTogglesTask = project.tasks.typedNamed<CollectProjectFeatureTogglesTask>(
