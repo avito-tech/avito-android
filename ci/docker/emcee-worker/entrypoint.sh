156d772ca0b651af515d57bef45a2f0eb9914603
RuslanMingaliev <mingaliev.rr@gmail.com>
2023-01-11 14:49:17 +0400

    MBSA-851 Remove port forwarding from Emcee worker

--- ci/docker/emcee-worker/entrypoint.sh
+++ ci/docker/emcee-worker/entrypoint.sh
@@ -2,12 +2,5 @@
 
 set -ex
 
-# Detect ip and forward ADB ports outside to outside interface
-# These ports have to be exposed in docker
-ip=$(ifconfig eth0 | grep 'inet' | cut -d: -f2 | awk '{ print $2}')
-socat tcp-listen:5037,bind=$ip,fork tcp:127.0.0.1:5037 &
-socat tcp-listen:5554,bind=$ip,fork tcp:127.0.0.1:5554 &
-socat tcp-listen:5555,bind=$ip,fork tcp:127.0.0.1:5555 &
-
 adb start-server
 java -jar ./emcee-worker.jar start -c config.json -ll info
