3e057a7a080060505fc4dc462c4623169b9209a2
Dmitriy Prokop <daprokop@avito.ru>
2023-01-20 14:43:11 +0300

    MTT-98: add emcee worker cloud config

--- ci/docker/emcee-worker/entrypoint.sh
+++ ci/docker/emcee-worker/entrypoint.sh
@@ -2,7 +2,17 @@
 
 set -ex
 
+CONFIG=config.json
+
+while getopts c: flag;
+do
+    case "${flag}" in
+        c) CONFIG=${OPTARG};;
+        *) echo "flag ${flag} not supported";;
+    esac
+done
+
 adb start-server
 
 # --add-opens solves the Retrofit and Java 11 issue. See https://github.com/square/retrofit/issues/3341
-java --add-opens=java.base/java.lang.invoke=ALL-UNNAMED -jar ./emcee-worker.jar start -c config.json -ll info
+java --add-opens=java.base/java.lang.invoke=ALL-UNNAMED -jar ./emcee-worker.jar start -c $CONFIG -ll info
