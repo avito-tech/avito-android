24fc4c85aa53dae0232ec9629d95de0ae5d0a993
Sergey Boishtyan <vitilus123@gmail.com>
2023-06-07 18:31:56 +0300

    MBSA-944 Delete isRunFromIde idk it's proposition

--- subprojects/logger/logger/src/main/kotlin/com/avito/logger/PrintlnLoggerFactory.kt
+++ subprojects/logger/logger/src/main/kotlin/com/avito/logger/PrintlnLoggerFactory.kt
@@ -4,11 +4,5 @@ import java.io.Serializable
 
 public object PrintlnLoggerFactory : LoggerFactory, Serializable {
 
-    private val isRunFromIde = System.getProperty("isInvokedFromIde") == "true"
-
-    override fun create(tag: String): Logger = if (isRunFromIde) {
-        PrintlnLogger(tag)
-    } else {
-        NoOpLogger
-    }
+    override fun create(tag: String): Logger = PrintlnLogger(tag)
 }
