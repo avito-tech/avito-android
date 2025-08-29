8c7cc4015ac6ff1d983f469e40171dfc61b87c8f
Ruslan Mingaliev <mingaliev.rr@gmail.com>
2025-08-29 18:43:52 +0700

    MBSA-1981 Add testrunner tag to TMCT build duration metric
    Signed-off-by: Ruslan Mingaliev <mingaliev.rr@gmail.com>

--- subprojects/teamcity-metrics-collector/src/main/kotlin/com/avito/teamcity/metric/TeamcityBuildDurationMetric.kt
+++ subprojects/teamcity-metrics-collector/src/main/kotlin/com/avito/teamcity/metric/TeamcityBuildDurationMetric.kt
@@ -17,11 +17,22 @@ internal class TeamcityBuildDurationMetric(
         val seriesName = base
             .addTag(key = "build_type", value = build.buildConfigurationId.stringId)
             .addTag(key = "status", value = status.lowercase())
+            .addTags(tags = additionalTags(build))
 
         return GraphiteMetric(
             seriesName,
             duration.seconds.toString(),
             build.startDateTime!!.toInstant(),
         )
+    }
+
+    private fun additionalTags(build: Build): Map<String, String> {
+        return build.parameters
+            .filter { parameter -> parameter.name.startsWith(METRIC_TAGS_PREFIX) }
+            .associate { parameter -> parameter.name.substringAfter(METRIC_TAGS_PREFIX) to parameter.value }
+    }
+
+    companion object {
+        private const val METRIC_TAGS_PREFIX = "metadata.metrics.tag."
     }
 }
