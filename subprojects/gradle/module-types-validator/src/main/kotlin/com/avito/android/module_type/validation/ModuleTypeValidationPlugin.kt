ebca493953efccfe28df676c64c1a44122c50d72
Pavel Glushen <pvglushen@avito.ru>
2026-02-16 16:31:52 +0300

    MA-6099 Get rid of Missing Fake Modules check

--- subprojects/gradle/module-types-validator/src/main/kotlin/com/avito/android/module_type/validation/ModuleTypeValidationPlugin.kt
+++ subprojects/gradle/module-types-validator/src/main/kotlin/com/avito/android/module_type/validation/ModuleTypeValidationPlugin.kt
@@ -1,7 +1,6 @@
 package com.avito.android.module_type.validation
 
 import com.avito.android.module_type.ModuleTypesPlugin
-import com.avito.android.module_type.validation.configurations.missings.implementations.MissingFakeModuleConfiguration
 import com.avito.android.module_type.validation.configurations.missings.implementations.MissingImplementationDependencyConfiguration
 import com.avito.android.module_type.validation.internal.hasModuleTypePlugin
 import com.avito.android.module_type.validation.internal.moduleTypeExtension
@@ -15,7 +14,6 @@ public class ModuleTypeValidationPlugin : Plugin<Project> {
 
     private val validationConfigurations = setOf(
         MissingImplementationDependencyConfiguration(),
-        MissingFakeModuleConfiguration(),
     )
 
     override fun apply(target: Project) {
--- subprojects/gradle/module-types-validator/src/main/kotlin/com/avito/android/module_type/validation/configurations/missings/implementations/MissingFakeModuleRootExtension.kt
+++
@@ -1,9 +0,0 @@
-package com.avito.android.module_type.validation.configurations.missings.implementations
-
-import org.gradle.api.provider.Property
-import java.io.File
-
-public abstract class MissingFakeModuleRootExtension {
-
-    public abstract val ignoreLogicalModuleRegexes: Property<File>
-}
